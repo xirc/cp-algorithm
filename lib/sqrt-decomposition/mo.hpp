615954af25a01c458f79b4b9914fc5e44284a9c6
xirc <xirckey@gmail.com>
2020-02-06 15:01:23 +0900

    lib/sqrt-decomposition/mo

---
+++ lib/sqrt-decomposition/mo.hpp
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <utility>
+#include <vector>
+#include <algorithm>
+
+const int S = 700; // Block Size
+
+struct Query {
+    int l, r, idx;
+    bool operator<(Query &rhs) {
+        return std::make_pair(l / S, r) < std::make_pair(rhs.l / S, rhs.r);
+    }
+};
+
+template <class T>
+class Mo {
+public:
+    // O((N+Q)sqrt(N))
+    std::vector<T> solve(std::vector<Query>& queries) {
+        std::vector<T> ans(queries.size());
+        std::sort(ans.begin(), ans.end());
+
+        int cl = 0, cr = -1;
+        for (auto query : queries) {
+            while (cl > query.l){
+                cl--;
+                add(cl);
+            }
+            while (cr < query.r) {
+                cr++;
+                add(cr);
+            }
+            while (cl < query.l) {
+                remove(cl);
+                cl++;
+            }
+            while (cr > query.r) {
+                remove(cr);
+                cr--;
+            }
+            ans[query.idx] = answer();
+        }
+        return ans;
+    }
+
+protected:
+    void remove(int idx);
+    void add(int idx);
+    T answer();
+};
