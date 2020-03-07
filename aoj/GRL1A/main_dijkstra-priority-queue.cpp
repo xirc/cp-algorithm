9b99ca787f32393bba441ceeac76716166de4304
xirc <xirckey@gmail.com>
2020-03-07 18:08:41 +0900

    aoj/GRL1A

---
+++ aoj/GRL1A/main_dijkstra-priority-queue.cpp
@@ -0,0 +1,89 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_1_A
+
+#include <iostream>
+#include <vector>
+#include <queue>
+
+class Dijkstra {
+    struct edge { int to; long long distance; };
+    int m_size;
+    std::vector<std::vector<edge>> adj;
+
+public:
+    static const long long inf = 1e18;
+
+    Dijkstra(int size) : m_size(size), adj(size) {
+    }
+
+    int size() {
+        return m_size;
+    }
+
+    void add_edge(int from, int to, long long distance) {
+        throw_if_invalid_index(from);
+        throw_if_invalid_index(to);
+        if (distance < 0) throw;
+        adj[from].push_back({ to, distance });
+    }
+
+    // O(V log V + E)
+    void solve(int s, std::vector<long long>& D, std::vector<int>& P) {
+        D.assign(m_size, inf);
+        P.assign(m_size, -1);
+
+        using pii = std::pair<int,int>;
+        std::priority_queue<pii, std::vector<pii>, std::greater<pii>> Q;
+        std::vector<bool> used(m_size, false);
+
+        D[s] = 0;
+        Q.push({0, s});
+
+        while (!Q.empty()) {
+            int v = Q.top().second; Q.pop();
+
+            if (used[v]) continue;
+            used[v] = true;
+
+            for (auto edge : adj[v]) {
+                int to = edge.to; long long len = edge.distance;
+                if (D[v] + len < D[to]) {
+                    D[to] = D[v] + len;
+                    P[to] = v;
+                    Q.push({D[to], to});
+                }
+            }
+        }
+    }
+
+private:
+    void throw_if_invalid_index(int index) {
+        if (index < 0 || index >= m_size) throw "index out of range";
+    }
+};
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int V, E, R;
+    cin >> V >> E >> R;
+
+    Dijkstra solver(V);
+    for (int i = 0; i < E; ++i) {
+        int u, v, w;
+        cin >> u >> v >> w;
+        solver.add_edge(u, v, w);
+    }
+
+    vector<long long> D;
+    vector<int> P;
+    solver.solve(R, D, P);
+    for (int i = 0; i < V; ++i) {
+        if (D[i] == Dijkstra::inf) cout << "INF" << endl;
+        else cout << D[i] << endl;
+    }
+
+    return 0;
+}
