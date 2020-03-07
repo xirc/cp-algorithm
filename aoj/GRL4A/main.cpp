2881da0dd7ebdb9cb59e128d582f95a13aa88ac2
xirc <xirckey@gmail.com>
2020-03-07 18:26:06 +0900

    aoj/GRL4A

---
+++ aoj/GRL4A/main.cpp
@@ -0,0 +1,94 @@
+// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_4_A
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+class Solver {
+    int m_size;
+    std::vector<std::vector<int>> adj;
+public:
+    Solver(int size): m_size(size), adj(size) {}
+    int size() {
+        return m_size;
+    }
+    void add_edge(int from, int to) {
+        throw_if_invalid_index(from);
+        throw_if_invalid_index(to);
+        adj[from].push_back(to);
+    }
+    // O(E)
+    bool solve(std::vector<int>& cycle) {
+        const int N = m_size;
+
+        std::vector<int> color(N, 0);
+        std::vector<int> parent(N, -1);
+        std::pair<int,int> be;
+        
+        bool found = false;
+        for (int v = 0; v < N; ++v) {
+            if (color[v] != 0) continue;
+            if (dfs(v, color, parent, be)) {
+                found = true;
+                break;
+            }
+        }
+
+        if (!found) {
+            return false;
+        }
+
+        cycle.clear();
+        cycle.push_back(be.first);
+        for (int v = be.second; v != be.first; v = parent[v]) {
+            cycle.push_back(v);
+        }
+        cycle.push_back(be.first);
+        reverse(cycle.begin(), cycle.end());
+        return true;
+    }
+private:
+    void throw_if_invalid_index(int index) {
+        if (index < 0 || index >= m_size) throw "index out of range";
+    }
+    bool dfs(int v, std::vector<int>& color, std::vector<int>& parent, std::pair<int,int>& be) {
+        color[v] = 1;
+        for (int u : adj[v]) {
+            if (color[u] == 0) {
+                parent[u] = v;
+                if (dfs(u, color, parent, be)) {
+                    return true;
+                }
+            } else if (color[u] == 1) {
+                be.first = u; // cycle begin
+                be.second = v; // cycle end
+                return true;
+            }
+        }
+        color[v] = 2;
+        return false;
+    }
+};
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int V, E;
+    cin >> V >> E;
+
+    Solver solver(V);
+    for (int i = 0; i < E; ++i) {
+        int u, v;
+        cin >> u >> v;
+        solver.add_edge(u, v);
+    }
+
+    vector<int> cycle;
+    bool has_cycle = solver.solve(cycle);
+    cout << (has_cycle ? 1 : 0) << endl;
+
+    return 0;
+}
