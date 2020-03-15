59346159442dd3b9a4ad128b501e53ab2e2a23b4
xirc <xirckey@gmail.com>
2020-03-15 22:13:11 +0900

    aoj/DSL{2B,2D,2F}

--- aoj/DSL2B/main_segment-tree.cpp
+++ aoj/DSL2B/main_segment-tree.cpp
@@ -1,115 +1,118 @@
 // http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_B
 
-#include <iostream>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+#include <functional>
 
 // SegmentTree (PointUpdate,RangeQuery)
 // Memory O(N)
 // Build  O(N)
 // Query O(log N)
 // Update O(log N)
-template<class T, class QueryMonoid, class UpdateMonoid>
+template<class T, class E>
 class SegmentTree {
+public:
+    using value_type_T = T;
+    using value_type_E = E;
+    using F = std::function<T(const T&, const T&)>;
+    using G = std::function<T(const T&, const E&)>;
+
 protected:
-    int m_size;
-    std::vector<T> m_array;
-    std::vector<T> m_tree;
+    F query_op;
+    T query_id;
+    G update_op;
+    int N, M;
+    std::vector<T> tree;
 
 public:
-    SegmentTree(int n)
+    // O(N)
+    SegmentTree(
+        int n,
+        const F& query_op, const T& query_id,
+        const G& update_op
+    )
+        : query_op(query_op)
+        , query_id(query_id)
+        , update_op(update_op)
     {
-        build(std::vector<T>(n, UpdateMonoid::id()));
+        build(std::vector<T>(n, query_id));
     }
-
+    template <class Query, class Update>
+    SegmentTree(int n, const Query& query, const Update& update)
+        : SegmentTree(
+            n,
+            std::bind(&Query::operator(), query, std::placeholders::_1, std::placeholders::_2),
+            query.id,
+            std::bind(&Update::operator(), update, std::placeholders::_1, std::placeholders::_2)
+        )
+    {
+        // Do nothing
+    }
     // O(1)
     int size() {
-        return m_size;
+        return N;
     }
-
     // O(N)
     void build(const std::vector<T>& array) {
-        m_size = array.size();
-        m_array.resize(m_size);
-        std::fill(m_array.begin(), m_array.end(), UpdateMonoid::id());
-        m_tree.resize(m_size * 4);
-        std::fill(m_tree.begin(), m_tree.end(), UpdateMonoid::id());
-        build(array, 0, 0, m_size);
+        N = array.size();
+        M = 4 * N;
+        tree.assign(M, query_id);
+        build(array, 0, 0, N);
     }
-
     // find the value in range [l, r)
     // O(log N)
     T query(int l, int r) {
-        return query(0, 0, m_size, std::max(l,0), std::min(r,m_size));
+        return query(0, 0, N, std::max(l,0), std::min(r,N));
     }
-
-    // update the value at index which is in range [0, n)
+    // update the value at index = [0, n)
     // O(log N)
-    bool update(int index, T value) {
-        if (index < 0 || index >= m_size) {
+    bool update(int index, const E& value) {
+        if (index < 0 || index >= N) {
             return false;
         }
-        update(0, 0, m_size, index, value);
+        update(0, 0, N, index, value);
         return true;
     }
-
-    // O(N)
+    // O(N logN)
     void dump(std::vector<T>& buffer) {
-        if (buffer.capacity() < m_size) {
-            buffer.reserve(m_size);
-        }
         buffer.clear();
-        for (int i = 0; i < m_size; ++i) {
-            buffer.push_back(m_array[i]);
+        for (int i = 0; i < N; ++i) {
+            buffer.push_back(query(i,i+1));
         }
     }
 
 protected:
-    static constexpr int left(int v) {
-        return v * 2 + 1;
-    }
-
-    static constexpr int right(int v) {
-        return v * 2 + 2;
-    }
-
+    static constexpr int left(int v) { return v * 2 + 1; }
+    static constexpr int right(int v) { return v * 2 + 2; }
     void build(const std::vector<T>& array, int v, int tl, int tr) {
-        if (tr - tl <= 0) {
-            return;
-        }
+        if (tr - tl <= 0) return;
         if (tr - tl == 1) {
-            m_array[tl] = array[tl];
-            m_tree[v] = UpdateMonoid::op(m_tree[v], array[tl]);
+            tree[v] = array[tl];
         } else {
             const int tm = (tl + tr) / 2;
             const int vl = left(v), vr = right(v);
             build(array, vl, tl, tm);
             build(array, vr, tm, tr);
-            m_tree[v] = QueryMonoid::op(m_tree[vl], m_tree[vr]);
+            tree[v] = query_op(tree[vl], tree[vr]);
         }
     }
-
     T query(int v, int tl, int tr, int l, int r) {
         if (l >= r) {
-            return QueryMonoid::id();
+            return query_id;
         }
         if (l == tl && r == tr) {
-            return m_tree[v];
+            return tree[v];
         }
         const int tm = (tl + tr) / 2;
         const int vl = left(v), vr = right(v);
         const auto sl = query(vl, tl, tm, l, std::min(r,tm));
         const auto sr = query(vr, tm, tr, std::max(l,tm), r);
-        return QueryMonoid::op(sl, sr);
+        return query_op(sl, sr);
     }
-
-    void update(int v, int tl, int tr, int index, T value) {
-        if (tr - tl <= 0) {
-            return;
-        }
+    void update(int v, int tl, int tr, int index, const E& value) {
+        if (tr - tl <= 0) return;
         if (tr - tl == 1) {
-            m_array[tl] = UpdateMonoid::op(m_array[tl], value);
-            m_tree[v] = UpdateMonoid::op(m_tree[v], value);
+            tree[v] = update_op(tree[v], value);
         } else {
             const int tm = (tl + tr) / 2;
             const int vl = left(v), vr = right(v);
@@ -118,20 +121,23 @@ protected:
             } else {
                 update(vr, tm, tr, index, value);
             }
-            m_tree[v] = QueryMonoid::op(m_tree[vl], m_tree[vr]);
+            tree[v] = query_op(tree[vl], tree[vr]);
         }
     }
 };
 
+
+#include <iostream>
+
 using namespace std;
 
 struct Add {
-    static int id() { return 0; }
-    static int op(int lhs, int rhs) { return lhs + rhs; }
+    const int id = 0;
+    int operator()(int lhs, int rhs) const { return lhs + rhs; };
 };
 struct Sum {
-    static int id() { return 0; }
-    static int op(int lhs, int rhs) { return lhs + rhs; }
+    const int id = 0;
+    int operator()(int lhs, int rhs) const { return lhs + rhs; }
 };
 
 int main() {
@@ -141,7 +147,7 @@ int main() {
     int N, Q;
     cin >> N >> Q;
 
-    SegmentTree<int,Sum,Add> tree(N);
+    SegmentTree<int,int> tree(N, Sum(), Add());
     for (int i = 0; i < Q; ++i) {
         int c, x, y;
         cin >> c >> x >> y;
