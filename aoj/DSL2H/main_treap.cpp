fab8cd9bd89ffeb99115d271d788038a47b86201
xirc <xirckey@gmail.com>
2020-03-17 18:04:35 +0900

    aoj/DSL2H

--- aoj/DSL2H/main_treap.cpp
+++ aoj/DSL2H/main_treap.cpp
@@ -1,17 +1,25 @@
 // http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=DSL_2_H
 
-#include <random>
 #include <vector>
+#include <functional>
+#include <random>
 
-template <class T>
-class TreapBase {
+template <class T, class E>
+class Treap {
+public:
+    using F = std::function<T(const T&, const T&, const T&)>;
+    using G = std::function<T(const T&, const E&)>;
+    using H = std::function<E(const E&, const E&)>;
+
 protected:
     struct Node {
         T key;
+        E lazy;
         int priority;
         Node *left, *right;
-        Node(T key, int priority)
+        Node(T key, E lazy, int priority)
             : key(key)
+            , lazy(lazy)
             , priority(priority)
             , left(nullptr)
             , right(nullptr)
@@ -22,12 +30,53 @@ protected:
     using Tree = Node*;
 
     std::mt19937 random;
-    Tree root = nullptr;
+    Tree root;
+    F query_op;
+    T query_id;
+    G update_op;
+    H lazy_op;
+    E lazy_id;
 
 public:
+    // O(1)
+    Treap(
+        const F& query_op,
+        const T& query_id,
+        const G& update_op,
+        const H& lazy_op,
+        const E& lazy_id
+    )
+        : root(nullptr)
+        , random()
+        , query_op(query_op)
+        , query_id(query_id)
+        , update_op(update_op)
+        , lazy_op(lazy_op)
+        , lazy_id(lazy_id)
+    {
+        // Do nothing
+    }
+    // O(1)
+    template <class Query, class Update, class Lazy>
+    Treap(const Query& query, const Update& update, const Lazy& lazy)
+        : Treap(
+            std::bind(&Query::operator(), query, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3),
+            query.id,
+            std::bind(&Update::operator(), update, std::placeholders::_1, std::placeholders::_2),
+            std::bind(&Lazy::operator(), lazy, std::placeholders::_1, std::placeholders::_2),
+            lazy.id
+        )
+    {
+        // Do nothing
+    }
+    // O(N)
+    void dump(std::vector<T>& buffer) {
+        buffer.clear();
+        dump(root, buffer);
+    }
     // O(logN)
     void insert(T key) {
-        insert(root, new Node(key, random()));
+        insert(root, new Node(key, lazy_id, random()));
     }
     // O(logN)
     bool erase(T key) {
@@ -37,33 +86,50 @@ public:
     bool find(T key) {
         return find(root, key);
     }
-    // O(N)
-    void dump(std::vector<T>& buffer) {
-        buffer.clear();
-        dump(root, buffer);
+    // O(logN)
+    T query(const T& l, const T& r) {
+        return query(root, l, r);
+    }
+    // O(logN)
+    bool update(const T& l, const T& r, const E& value) {
+        return update(root, l, r, value);
     }
 
 protected:
-    virtual void pushup(Tree tree) {
-        // Do pushup procedure.
+    void pushup(Tree tree) {
+        if (tree) {
+            auto lkey = tree->left ? tree->left->key : query_id;
+            auto rkey = tree->right ? tree->right->key : query_id;
+            tree->key = query_op(tree->key, lkey, rkey);
+        }
     }
-    virtual void pushdown(Tree tree) {
-        // Do pushdown procedure.
+    void pushdown(Tree tree) {
+        if (tree) {
+            if (tree->left) {
+                tree->left->key = update_op(tree->left->key, tree->lazy);
+                tree->left->lazy = lazy_op(tree->left->lazy, tree->lazy);
+            }
+            if (tree->right) {
+                tree->right->key = update_op(tree->right->key, tree->lazy);
+                tree->right->lazy = lazy_op(tree->right->lazy, tree->lazy);
+            }
+            tree->lazy = lazy_id;
+        }
     }
     // O(logN)
-    // left: <= key, right: > key
+    // left: < key, right: >= key
     void split(Tree tree, T key, Tree& left, Tree& right) {
         if (!tree) {
             left = right = nullptr;
             return;
         }
         pushdown(tree);
-        if (key < tree->key) {
-            split(tree->left, key, left, tree->left);
-            right = tree;
-        } else {
+        if (tree->key < key) {
             split(tree->right, key, tree->right, right);
             left = tree;
+        } else {
+            split(tree->left, key, left, tree->left);
+            right = tree;
         }
         pushup(tree);
     }
@@ -81,6 +147,16 @@ protected:
             tree = right;
         }
         pushup(tree);
+    }
+    // O(N)
+    void dump(Tree tree, std::vector<T>& buffer) {
+        if (!tree) {
+            return;
+        }
+        pushdown(tree);
+        dump(tree->left, buffer);
+        buffer.push_back(tree->key);
+        dump(tree->right, buffer);
     }
     // O(logN)
     void insert(Tree &tree, Tree item) {
@@ -104,11 +180,13 @@ protected:
         }
         bool has_key;
         pushdown(tree);
-        if (tree->key == key) {
+        if (key < tree->key) {
+            has_key = erase(tree->left, key);
+        } else if (tree->key < key) {
+            has_key = erase(tree->right, key);
+        } else {
             merge(tree, tree->left, tree->right);
             has_key = true;
-        } else {
-            has_key = erase(key < tree->key ? tree->left : tree->right, key);
         }
         pushup(tree);
         return has_key;
@@ -119,21 +197,41 @@ protected:
             return false;
         }
         pushdown(tree);
-        if (tree->key == key) {
-            return true;
+        if (key < tree->key) {
+            return find(tree->left, key);
+        } else if (tree->key < key) {
+            return find(tree->right, key);
         } else {
-            return find(key < tree->key ? tree->left : tree->right, key);
+            return true;
         }
     }
-    // O(N)
-    void dump(Tree tree, std::vector<T>& buffer) {
-        if (!tree) {
-            return;
+    // O(logN)
+    // [l,r)
+    T query(Tree &tree, const T &l, const T& r) {
+        Tree t1, t2, t3;
+        T ans;
+        split(tree, l, t1, t2);
+        split(t2, r, t2, t3);
+        ans = t2 ? t2->key : query_id;
+        merge(t2, t2, t3);
+        merge(tree, t1, t2);
+        return ans;
+    }
+    // O(logN)
+    // [l,r)
+    bool update(Tree &tree, const T& l, const T& r, const E& value) {
+        Tree t1, t2, t3;
+        bool has_key = false;
+        split(tree, l, t1, t2);
+        split(t2, r, t2, t3);
+        if (t2) {
+            has_key = true;
+            t2->key = update_op(t2->key, value);
+            t2->lazy = lazy_op(t2->lazy, value);
         }
-        pushdown(tree);
-        dump(tree->left, buffer);
-        buffer.push_back(tree->key);
-        dump(tree->right, buffer);
+        merge(t2, t2, t3);
+        merge(tree, t1, t2);
+        return has_key;
     }
 };
 
@@ -150,58 +248,31 @@ struct Data {
         return index < other.index;
     }
 };
-class Treap : public TreapBase<Data> {
-public:
-    // [l,r)
-    void add(int l, int r, long long x) {
-        Tree t1, t2, t3;
-        split(root, { l - 1, 0, 0, 0 }, t1, t2);
-        split(t2, { r - 1, 0, 0, 0 }, t2, t3);
-        if (t2) {
-            t2->key.value += x;
-            t2->key.lazy += x;
-            t2->key.minimum += x;
-        }
-        merge(t2, t2, t3);
-        merge(root, t1, t2);
-    }
-    // [l,r)
-    long long minimum(int l, int r) {
-        Tree t1, t2, t3;
-        long long ans;
-        split(root, { l - 1, 0, 0, 0 }, t1, t2);
-        split(t2, { r - 1, 0, 0, 0 }, t2, t3);
-        ans = minimum(t2);
-        merge(t2, t2, t3);
-        merge(root, t1, t2);
+struct Query {
+    const Data id = { 0, 0, 0, std::numeric_limits<long long>::max() };
+    Data operator()(const Data& node, const Data& lhs, const Data& rhs) const {
+        Data ans;
+        ans.index = node.index;
+        ans.value = node.value;
+        ans.lazy = 0;
+        ans.minimum = min({ node.value, lhs.minimum, rhs.minimum });
         return ans;
     }
-protected:
-    long long minimum(Tree node) {
-        return node ? node->key.minimum : numeric_limits<long long>::max();
+};
+struct Update {
+    Data operator()(const Data& lhs, long long rhs) const {
+        Data ans;
+        ans.index = lhs.index;
+        ans.value = lhs.value + rhs;
+        ans.lazy = 0;
+        ans.minimum = lhs.minimum + rhs;
+        return ans;
     }
-    void pushup(Tree node) override {
-        if (!node) return;
-
-        node->key.minimum = min({ node->key.value, minimum(node->left), minimum(node->right) });
-    }
-    void pushdown(Tree node) override {
-        if (!node) return;
-
-        auto lazy = node->key.lazy;
-        auto& left = node->left;
-        auto& right = node->right;
-        if (left) {
-            left->key.value += lazy;
-            left->key.lazy += lazy;
-            left->key.minimum += lazy;
-        }
-        if (right) {
-            right->key.value += lazy;
-            right->key.lazy += lazy;
-            right->key.minimum += lazy;
-        }
-        node->key.lazy = 0;
+};
+struct Lazy {
+    const long long id = 0;
+    long long operator()(long long lhs, long long rhs) const {
+        return lhs + rhs;
     }
 };
 
@@ -212,7 +283,8 @@ int main() {
     int N, Q;
     cin >> N >> Q;
 
-    Treap treap;
+    auto query = Query(); auto update = Update(); auto lazy = Lazy();
+    Treap<Data,long long> treap(query, update, lazy);
     for (int i = 0; i < N; ++i) {
         treap.insert({ i, 0, 0, 0 });
     }
@@ -222,10 +294,10 @@ int main() {
         cin >> c;
         if (c == 0 ) {
             cin >> s >> t >> x;
-            treap.add(s, t+1, x);
+            treap.update({ s, 0, 0, 0}, { t+1, 0, 0, 0 }, x);
         } else if (c == 1) {
             cin >> s >> t;
-            cout << treap.minimum(s, t+1) << endl;
+            cout << treap.query({ s, 0, 0, 0}, { t+1, 0, 0, 0 }).minimum << endl;
         } else throw;
     }
 
