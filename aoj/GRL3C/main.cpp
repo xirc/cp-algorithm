077c57592d4973ca65855b789349789a9e0c8a62
xirc <xirckey@gmail.com>
2020-03-19 20:36:19 +0900

    aoj/GRL3C

---
+++ aoj/GRL3C/main.cpp
@@ -0,0 +1,122 @@
+#include <vector>
+#include <stack>
+#include <algorithm>
+
+// Strongly Connected Components
+// Memory: O(E + V)
+class SCC {
+    int N;
+    std::vector<std::vector<int>> G;
+    std::vector<std::vector<int>> GT;
+
+public:
+    // O(N)
+    SCC(int size): N(size), G(size), GT(size) {}
+    // O(1)
+    int size() {
+        return N;
+    }
+    // O(1)
+    void add_edge(int from, int to) {
+        throw_if_invalid_index(from);
+        throw_if_invalid_index(to);
+        G[from].push_back(to);
+        GT[to].push_back(from);
+    }
+    // O(V + E)
+    // Kosaraju's Algorithm
+    // ans[u] = Component ID of vertex 'u'.
+    // Component ID is assigned in topological order.
+    std::vector<int> solve() {
+        std::stack<std::pair<int,int>> S;
+        std::vector<int> ans(N, -1);
+        std::vector<bool> used(N, false);
+        std::vector<int> order;
+
+        order.reserve(N);
+        for (int s = 0; s < N; ++s) {
+            if (used[s]) continue;
+            S.push({s,-1});
+            while (S.size()) {
+                int u = S.top().first;
+                int& i = S.top().second;
+                if (i < 0) {
+                    used[u] = true;
+                } else if (i < G[u].size()) {
+                    int v = G[u][i];
+                    if (!used[v]) {
+                        S.push({ v, -1 });
+                    }
+                } else {
+                    order.push_back(u);
+                    S.pop();
+                }
+                ++i;
+            }
+        }
+        std::reverse(order.begin(), order.end());
+
+        int c = 0;
+        for (int i = 0; i < N; ++i) {
+            int s = order[i];
+            if (ans[s] != -1) continue;
+            S.push({ s, -1 });
+            while (S.size()) {
+                int u = S.top().first;
+                int& j = S.top().second;
+                if (j < 0) {
+                    ans[u] = c;
+                } else if (j < GT[u].size()) {
+                    int v = GT[u][j];
+                    if (ans[v] == -1) {
+                        S.push({ v, -1 });
+                    }
+                } else {
+                    S.pop();
+                }
+                ++j;
+            }
+            ++c;
+        }
+
+        return ans;
+    }
+
+private:
+    void throw_if_invalid_index(int index) {
+        if (index < 0 || index >= N) throw "index out of range";
+    }
+};
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int V, E, Q;
+    SCC solver(0);
+
+    cin >> V >> E;
+    solver = SCC(V);
+    for (int i = 0; i < E; ++i) {
+        int s, t;
+        cin >> s >> t;
+        solver.add_edge(s, t);
+    }
+
+    auto com = solver.solve();
+
+    cin >> Q;
+    for (int i = 0; i < Q; ++i) {
+        int u, v;
+        cin >> u >> v;
+        auto ans = com[u] == com[v] ? 1 : 0;
+        cout << ans << endl;
+    }
+
+    return 0;
+}
