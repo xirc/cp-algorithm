1be0ee53e2c843834293394ba79ee40f1e411736
xirc <xirckey@gmail.com>
2020-03-21 17:33:04 +0900

    lib/graph_articulation_points

---
+++ lib/graph_articulation-points/main-graph-articulation-points.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include "../template-main.hpp"
+#include "./graph-articulation-points.hpp"
+
+using namespace std;
+
+ArticulationPoints solver(0);
+
+void action_init() {
+    int size;
+    cin >> size;
+    if (size < 0) {
+        cout << "false" << endl;
+        return;
+    }
+    solver = ArticulationPoints(size);
+    cout << "true" << endl;
+}
+
+void action_edge() {
+    int u, v;
+    cin >> u >> v;
+    if (u < 0 || u >= solver.size() ||
+        v < 0 || v >= solver.size())
+    {
+        cout << "false" << endl;
+        return;
+    }
+    solver.add_edge(u, v);
+    cout << "true" << endl;
+}
+
+void action_solve() {
+    auto cutpoints = solver.solve();
+    for (int i = 0; i < cutpoints.size(); ++i) {
+        if (i > 0) cout << " ";
+        cout << cutpoints[i];
+    }
+    cout << endl;
+}
+
+void setup(string& header, map<string,Command>& commands) {
+    header = "Articulation Points of a Graph";
+    commands["init"] = { "init {size}", action_init };
+    commands["edge"] = { "edge {u} {v}", action_edge };
+    commands["solve"] = { "solve", action_solve };
+}
