ac24b42e0a30b38fd9870e91489244a5284865f7
xirc <xirckey@gmail.com>
2020-05-19 14:48:36 +0900

    aoj/CGL3A

---
+++ aoj/CGL/CGL3A/main.cpp
@@ -0,0 +1,111 @@
+#include <bits/stdc++.h>
+
+struct vector2 {
+    double x, y;
+    vector2() {}
+    vector2(double x, double y): x(x), y(y) {}
+    vector2& operator+=(const vector2& o) {
+        x += o.x;
+        y += o.y;
+        return *this;
+    }
+    vector2& operator-=(const vector2& o) {
+        x -= o.x;
+        y -= o.y;
+        return *this;
+    }
+    vector2& operator*=(const double& o) {
+        x *= o;
+        y *= o;
+        return *this;
+    }
+    vector2& operator/=(const double& o) {
+        x /= o;
+        y /= o;
+        return *this;
+    }
+    vector2 operator+(const vector2& o) const {
+        return vector2(*this) += o;
+    }
+    vector2 operator-(const vector2& o) const {
+        return vector2(*this) -= o;
+    }
+    vector2 operator*(const double& o) const {
+        return vector2(*this) *= o;
+    }
+    vector2 operator/(const double& o) const {
+        return vector2(*this) *= o;
+    }
+    double norm() const {
+        return x * x + y * y;
+    }
+    double length() const {
+        return std::sqrt(norm());
+    }
+    vector2 normalized() const {
+        return vector2(*this) /= length();
+    }
+    // Rotate (Counter Clockwise)
+    vector2 rotated(double rad) const {
+        auto o = vector2(std::cos(rad), std::sin(rad));
+        return vector2(
+            x * o.x - y * o.y,
+            x * o.y + y * o.x
+        );
+    }
+    // Unit Normal Vector (Counter Clockwise)
+    vector2 unit_normal() const {
+        return rotated(M_PI_2).normalized();
+    }
+    // Unit Normal Vector (Clockwise)
+    vector2 unit_normal_cw() const {
+        return rotated(-M_PI_2).normalized();
+    }
+};
+vector2 operator*(const double& o, const vector2& v) {
+    return v * o;
+}
+
+// Length of Vector
+double abs(const vector2& a) {
+    return a.length();
+}
+// dot(A,B) = dot(B,A)
+// dot(k * A,B) = k * dot(A,B)
+// dot(A + B, C) = dot(A, C) + dot(B, C)
+double dot(const vector2& a, const vector2& b) {
+    return a.x * b.x + a.y * b.y;
+}
+// Area of parallelogram formed by 'A' and 'B'
+// The value will be positive if rotation from A to B is counter-clockwise and negative otherwise.
+double cross(const vector2& a, const vector2& b) {
+    return a.x * b.y - a.y * b.x;
+}
+
+// Area of Polygon (can handle non-Convex)
+double polygon_area(const std::vector<vector2>& G) {
+    // The coordinates of points are given in the order of counter-clockwise visit of them.
+    double ans = 0;
+    for (int i = 0; i < G.size(); ++i) {
+        int j = (i + 1) % G.size();
+        ans += cross(G[i], G[j]);
+    }
+    return ans / 2;
+}
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int N;
+    cin >> N;
+    vector<vector2> G(N);
+    for (int i = 0; i < N; ++i) {
+        cin >> G[i].x >> G[i].y;
+    }
+    cout << fixed << setprecision(1) << polygon_area(G) << endl;
+
+    return 0;
+}
