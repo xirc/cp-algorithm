1bef07d2cf496a75147442e584d7dfb37d2618e2
xirc <xirckey@gmail.com>
2020-05-19 17:04:33 +0900

    Add compile check for lib/geometry.

---
+++ lib/geometry/main-vector3.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "../template-main.hpp"
+#include "./vector3.hpp"
+
+using namespace std;
+
+void setup(string& header, map<string,Command>& commands) {
+    header = "vector3";
+    // Do nothing, Just check compilable.
+}
