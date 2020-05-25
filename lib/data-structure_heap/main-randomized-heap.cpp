854ce937eb7aac9e834f34203c984d591274f997
xirc <xirckey@gmail.com>
2020-05-25 23:23:14 +0900

    Support update operator in Heap.

--- lib/data-structure_heap/main-randomized-heap.cpp
+++ lib/data-structure_heap/main-randomized-heap.cpp
@@ -39,7 +39,7 @@ void action_merge() {
         cout << "false" << endl;
         return;
     }
-    Heap<int> other;
+    auto other = Heap<int>();
     for (int i = 0; i < size; ++i) {
         int value;
         cin >> value;
@@ -49,6 +49,12 @@ void action_merge() {
     cout << "true" << endl;
 }
 
+void action_update() {
+    int value;
+    cin >> value;
+    heap.update(value);
+}
+
 void setup(string& header, map<string,Command>& commands) {
     header = "Heap";
     commands["init"] = { "init", action_init };
@@ -56,4 +62,5 @@ void setup(string& header, map<string,Command>& commands) {
     commands["top"] = { "top", action_top };
     commands["pop"] = { "pop", action_pop };
     commands["merge"] = { "merge {size} {...}", action_merge };
+    commands["update"] = { "update {value}", action_update };
 }
