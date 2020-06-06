8f3c9379bbafc6312b297964e7e72aa39b55cd18
xirc <xirckey@gmail.com>
2020-06-07 00:06:25 +0900

    Verify lib/util/dice

--- lib/util_dice/dice.hpp
+++ lib/util_dice/dice.hpp
@@ -3,6 +3,7 @@
 // Verified
 // https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/11/ITP1_11_A
 // https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/11/ITP1_11_B
+// https://onlinejudge.u-aizu.ac.jp/courses/lesson/2/ITP1/11/ITP1_11_C
 
 #include <vector>
 
