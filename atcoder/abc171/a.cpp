2ff4f2849b8fdcb77678225f5a0003cd92cd2b2f
xirc <xirckey@gmail.com>
2020-06-21 22:42:58 +0900

    atcoder/abc171a

---
+++ atcoder/abc171/a.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    char c;
+    cin >> c;
+    if (islower(c)) {
+        cout << "a" << endl;
+    } else {
+        cout << "A" << endl;
+    }
+
+    return 0;
+}
