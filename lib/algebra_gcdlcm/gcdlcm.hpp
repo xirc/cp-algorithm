fe58fed73f56c80b2e0b745e17603750a0624d4b
xirc <xirckey@gmail.com>
2020-06-28 13:47:56 +0900

    Verify lib/algebra/lcm

--- lib/algebra_gcdlcm/gcdlcm.hpp
+++ lib/algebra_gcdlcm/gcdlcm.hpp
@@ -17,6 +17,7 @@ long long gcd(long long a, long long b) {
 // LCM (a,b)
 // Time: O( log(min(a,b)) )
 // Verified: https://www.codechef.com/problems/FLOW016
+// Verified: https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_C
 long long lcm(long long a, long long b) {
     return a / gcd(a, b) * b;
 }
