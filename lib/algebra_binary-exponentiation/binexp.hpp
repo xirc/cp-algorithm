df9f947e5b157737280de10d2af0b7f5a30592a8
xirc <xirckey@gmail.com>
2020-06-28 14:29:48 +0900

    Revise lib/algebra/binexp

--- lib/algebra_binary-exponentiation/binexp.hpp
+++ lib/algebra_binary-exponentiation/binexp.hpp
@@ -1,18 +1,25 @@
 #pragma once
 
+#include <functional>
+
 // Binary Exponentation
 // Time: O(logN)
 // Memory: O(1)
 template<class T>
-T binpow(T a, long long n, const T& id) {
+T binpow(
+    T a,
+    long long n,
+    const T& id,
+    const std::function<T(const T&, const T&)> op = std::multiplies<T>()
+) {
     if (n < 0) throw;
 
     T ans = id;
     while (n > 0) {
         if (n & 1) {
-            ans = ans * a;
+            ans = op(ans, a);
         }
-        a = a * a;
+        a = op(a, a);
         n >>= 1;
     }
     return ans;
