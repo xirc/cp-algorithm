83a6c5e83da4de26cfe29c8bd802c541c041e249
Taichi Yamakawa <xirckey@gmail.com>
2020-10-23 23:44:26 +0900

    atcoder/abc119A

---
+++ atcoder/abc119/A/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    string S;
+    cin >> S;
+    if (S <= "2019/04/30") {
+        cout << "Heisei" << endl;
+    } else {
+        cout << "TBD" << endl;
+    }
+
+    return 0;
+}
