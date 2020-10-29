b80ad5a8eec95c5869d49120cd2f94c2f2c77965
Taichi Yamakawa <xirckey@gmail.com>
2020-10-29 23:50:16 +0900

    atcoder/abc113A

---
+++ atcoder/abc113/A/main.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int X, Y;
+    cin >> X >> Y;
+    cout << (X + Y / 2) << endl;
+
+    return 0;
+}
