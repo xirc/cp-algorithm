eed0968261f2a97cdff8e1266c61be285e6c8584
Taichi Yamakawa <xirckey@gmail.com>
2020-11-06 21:00:58 +0900

    Add Binary GCD as library code

--- lib/algebra_gcdlcm/gcdlcm.hpp
+++ lib/algebra_gcdlcm/gcdlcm.hpp
@@ -3,7 +3,7 @@
 #include <bits/stdc++.h>
 
 // GCD (a,b)
-// Time: O( log(min(a,b) )
+// Time: O( log(min(a,b)) )
 // Verified: https://www.codechef.com/problems/FLOW016
 // Verified: https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/1/ALDS1_1_B
 long long gcd(long long a, long long b) {
@@ -20,4 +20,29 @@ long long gcd(long long a, long long b) {
 // Verified: https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_C
 long long lcm(long long a, long long b) {
     return a / gcd(a, b) * b;
+}
+
+
+// Binary GCD (a,b)
+// Time: O ( log(min(a,b)) )
+// The optimized Euclidean algorithm.
+// The time complexity is same, but a lot faster than normal one that is using modulo operation.
+long long bingcd(long long a, long long b) {
+    if (a == 0) return b;
+    if (b == 0) return a;
+
+    long long g = 1;
+    while (b > 0) {
+        if (a & 1 == 0 && b & 1 == 0) {
+            g <<= 1;
+        } else if (a & 1 == 0) {
+            a >>= 1;
+        } else if (b & 1 == 0) {
+            b >>= 1;
+        } else {
+            if (a < b) std::swap(a, b);
+            a -= b;
+        }
+    }
+    return g * a;
 }
