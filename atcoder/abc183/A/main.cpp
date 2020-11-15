3f6bfbcdedcdcfacfdd9031070b570eface3af88
Taichi Yamakawa <xirckey@gmail.com>
2020-11-15 22:42:00 +0900

    atcoder/abc183A

---
+++ atcoder/abc183/A/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int x;
+    cin >> x;
+    if (x >= 0) {
+        cout << x << endl;
+    } else {
+        cout << 0 << endl;
+    }
+
+    return 0;
+}
