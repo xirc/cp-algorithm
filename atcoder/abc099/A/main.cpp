1020d18a72b3de112ccc99159e7a7a170c8da1b1
Taichi Yamakawa <xirckey@gmail.com>
2020-11-30 22:11:19 +0900

    atcoder/abc099A

---
+++ atcoder/abc099/A/main.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int N;
+    cin >> N;
+    if (N < 1000) {
+        cout << "ABC" << endl;
+    } else {
+        cout << "ABD" << endl;
+    }
+
+    return 0;
+}
