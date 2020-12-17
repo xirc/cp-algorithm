fd6f5a2ccd66a4abaaeead677f290078b69dcdeb
Taichi Yamakawa <xirckey@gmail.com>
2020-12-18 00:06:30 +0900

    atcoder/abc085A

---
+++ atcoder/abc085/A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    string S;
+    cin >> S;
+    S[3] = '8';
+    cout << S << endl;
+
+    return 0;
+}
