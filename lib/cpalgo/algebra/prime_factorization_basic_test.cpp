3c6f23d283522967d1798390147c7d9022bb5ac5
Taichi Yamakawa <xirckey@gmail.com>
2020-12-26 17:46:34 +0900

    Add tests for "prime_factorization_basic"

---
+++ lib/cpalgo/algebra/prime_factorization_basic_test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+#include "gtest/gtest.h"
+#include "prime_factorization_basic.hpp"
+
+
+TEST(PrimeFactorizationBasicTest, ShouldFactorize) {
+    std::vector<unsigned long long> factors_12 = { 2, 2, 3 };
+    EXPECT_EQ(prime_factorization(12), factors_12);
+
+    std::vector<unsigned long long> factors_126 = { 2, 3, 3, 7 };
+    EXPECT_EQ(prime_factorization(126), factors_126);
+}
