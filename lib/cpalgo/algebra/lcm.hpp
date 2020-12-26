de3372cb8c16d0d297994141209a296d6594c6b1
Taichi Yamakawa <xirckey@gmail.com>
2020-12-26 17:22:21 +0900

    "lcm" should handle zeros

--- lib/cpalgo/algebra/lcm.hpp
+++ lib/cpalgo/algebra/lcm.hpp
@@ -9,5 +9,6 @@
 // Verified: https://www.codechef.com/problems/FLOW016
 // Verified: https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_C
 unsigned long long lcm(unsigned long long a, unsigned long long b) {
+    if (a == 0 && b == 0) return 0;
     return a / gcd(a, b) * b;
 }
