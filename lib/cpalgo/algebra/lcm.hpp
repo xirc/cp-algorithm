e445a2d99fccf75dab16dc100588d3dab47e4b60
Taichi Yamakawa <xirckey@gmail.com>
2020-12-26 14:56:25 +0900

    Revise comments of "lcm"

--- lib/cpalgo/algebra/lcm.hpp
+++ lib/cpalgo/algebra/lcm.hpp
@@ -5,6 +5,7 @@
 
 // LCM (a,b)
 // Time: O( log(min(a,b)) )
+// See: https://cp-algorithms.com/algebra/euclid-algorithm.html
 // Verified: https://www.codechef.com/problems/FLOW016
 // Verified: https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_C
 long long lcm(long long a, long long b) {
