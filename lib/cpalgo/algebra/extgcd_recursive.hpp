2051e66451bb772fddd4295666c6b872d2ec2ab3
Taichi Yamakawa <xirckey@gmail.com>
2020-12-26 18:26:56 +0900

    "extgcd" should take unsigned numbers as coefficients

--- lib/cpalgo/algebra/extgcd_recursive.hpp
+++ lib/cpalgo/algebra/extgcd_recursive.hpp
@@ -22,13 +22,13 @@
 //  - https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_E
 //  - https://uva.onlinejudge.org/index.php?option=com_onlinejudge&Itemid=8&page=show_problem&problem=1045
 //
-inline long long extgcd(long long a, long long b, long long &x, long long &y) {
+inline unsigned long long extgcd(unsigned long long a, unsigned long long b, long long &x, long long &y) {
     if (b == 0) {
         x = 1, y = 0;
         return a;
     }
     long long x1, y1;
-    long long g = extgcd(b, a % b, x1, y1);
+    unsigned long long g = extgcd(b, a % b, x1, y1);
     x = y1;
     y = x1 - y1 * (a / b);
     return g;
--- lib/cpalgo/algebra/tests/extgcd_iterative_test.cpp
+++ lib/cpalgo/algebra/tests/extgcd_iterative_test.cpp
@@ -5,25 +5,25 @@
 
 TEST(ExtgcdIterativeTest, ShouldComputeExtendedEuclidean) {
     long long x, y;
-    long long g;
+    unsigned long long g;
 
     g = extgcd(4, 12, x, y);
-    EXPECT_EQ(1LL, x) << "  where 4 * x + 12 * y = g";
-    EXPECT_EQ(0LL, y) << "  where 4 * x + 12 * y = g";
-    EXPECT_EQ(4LL, g) << "  where 4 * x + 12 * y = g";
+    EXPECT_EQ(1LL,  x) << "  where 4 * x + 12 * y = g";
+    EXPECT_EQ(0LL,  y) << "  where 4 * x + 12 * y = g";
+    EXPECT_EQ(4ULL, g) << "  where 4 * x + 12 * y = g";
 
     g = extgcd(3, 8, x, y);
-    EXPECT_EQ( 3LL, x) << "  where 3 * x + 8 * y = g";
-    EXPECT_EQ(-1LL, y) << "  where 3 * x + 8 * y = g";
-    EXPECT_EQ( 1LL, g) << "  where 3 * x + 8 * y = g";
+    EXPECT_EQ( 3LL,  x) << "  where 3 * x + 8 * y = g";
+    EXPECT_EQ(-1LL,  y) << "  where 3 * x + 8 * y = g";
+    EXPECT_EQ( 1ULL, g) << "  where 3 * x + 8 * y = g";
 
     g = extgcd(4, 6, x, y);
-    EXPECT_EQ(-1LL, x) << "  where 4 * x + 6 * y = g";
-    EXPECT_EQ( 1LL, y) << "  where 4 * x + 6 * y = g";
-    EXPECT_EQ( 2LL, g) << "  where 4 * x + 6 * y = g";
+    EXPECT_EQ(-1LL,  x) << "  where 4 * x + 6 * y = g";
+    EXPECT_EQ( 1LL,  y) << "  where 4 * x + 6 * y = g";
+    EXPECT_EQ( 2ULL, g) << "  where 4 * x + 6 * y = g";
 
     g = extgcd(17, 17, x, y);
-    EXPECT_EQ( 0LL, x) << "  where 17 * x + 17 * y = g";
-    EXPECT_EQ( 1LL, y) << "  where 17 * x + 17 * y = g";
-    EXPECT_EQ(17LL, g) << "  where 17 * x + 17 * y = g";
+    EXPECT_EQ( 0LL,  x) << "  where 17 * x + 17 * y = g";
+    EXPECT_EQ( 1LL,  y) << "  where 17 * x + 17 * y = g";
+    EXPECT_EQ(17ULL, g) << "  where 17 * x + 17 * y = g";
 }
