0ee174801684d61996f6acc262e202f7758da267
Taichi Yamakawa <xirckey@gmail.com>
2020-12-26 17:46:02 +0900

    "prime_factorization_basic" should take unsigned numbers

--- lib/cpalgo/algebra/prime_factorization_basic.hpp
+++ lib/cpalgo/algebra/prime_factorization_basic.hpp
@@ -13,9 +13,9 @@
 // Verified:
 //  - https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/1/NTL_1_A
 //
-inline std::vector<long long> prime_factorization(long long M) {
-    std::vector<long long> factors;
-    for (long long i = 2; i * i < M; ++i) {
+inline std::vector<unsigned long long> prime_factorization(unsigned long long M) {
+    std::vector<unsigned long long> factors;
+    for (unsigned long long i = 2; i * i < M; ++i) {
         while (M % i == 0) {
             factors.push_back(i);
             M /= i;
