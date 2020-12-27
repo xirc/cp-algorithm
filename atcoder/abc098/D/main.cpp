b2b87faf7d917e6ceee14be2ce5e6fde15e87af5
Taichi Yamakawa <xirckey@gmail.com>
2020-12-27 23:33:45 +0900

    atcoder/abc098D

--- atcoder/abc098/D/main.cpp
+++ atcoder/abc098/D/main.cpp
@@ -1,5 +1,28 @@
 #include <bits/stdc++.h>
 
+inline void iterate_with_two_pointers(
+    size_t N,
+    // O(K), [l,r), l = [0,N), r = [0, N)
+    std::function<bool(size_t, size_t)> should_move_right,
+    // O(K), [l,r), l = [0,N), r = [0, N)
+    std::function<void(size_t, size_t)> pre_move_left,
+    // O(K), [l,r), l = [0,N), r = [0, N)
+    std::function<void(size_t, size_t)> pre_move_right,
+    // O(K), [l,r), l = [0,N), r = [0, N)
+    std::function<void(size_t, size_t)> update
+) {
+    size_t r = 0;
+    for (size_t l = 0; l < N; ++l) {
+        while (r < N && should_move_right(l,r)) {
+            pre_move_right(l, r);
+            ++r;
+        }
+        update(l, r);
+        if (l == r) ++r;
+        else pre_move_left(l, r);
+    }
+}
+
 using namespace std;
 using ll = long long;
 
@@ -8,28 +31,22 @@ vector<int> A;
 
 ll solve() {
     ll ans = 0;
-    ll count = 0;
     ll x = 0;
-    int l = 0, r = 0;
-    while (l < N) {
-        if (r < l) {
-            ++r;
-            continue;
-        }
-        if (r == N || x & A[r]) {
-            ans += count;
+    iterate_with_two_pointers(
+        N,
+        [&](size_t l, size_t r) {
+            return (x & A[r]) == 0;
+        },
+        [&](size_t l, size_t r) {
             x ^= A[l];
-            ++l;
-            if (l <= r) {
-                --count;
-            }
-            continue;
+        },
+        [&](size_t l, size_t r) {
+            x ^= A[r];
+        },
+        [&](size_t l, size_t r) {
+            ans += (r - l);
         }
-        ++count;
-        x ^= A[r];
-        ++r;
-    }
-    ans += count;
+    );
     return ans;
 }
 
