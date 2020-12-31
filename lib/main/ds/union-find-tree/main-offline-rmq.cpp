354e12ab190034c14a04454e00928f69040ba7e4
Taichi Yamakawa <xirckey@gmail.com>
2020-12-31 15:52:40 +0900

    Refactor main-offline-rmq example

--- lib/main/ds/union-find-tree/main-offline-rmq.cpp
+++ lib/main/ds/union-find-tree/main-offline-rmq.cpp
@@ -1,18 +1,15 @@
-#include <iostream>
-#include <random>
-#include <vector>
-#include <stack>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void union_find_tree_init(vector<int>& tree) {
-    for (int i = 0; i < tree.size(); ++i) {
+void union_find_tree_init(vector<size_t>& tree) {
+    for (size_t i = 0; i < tree.size(); ++i) {
         tree[i] = i;
     }
 }
 
-int union_find_tree_find_set(vector<int>& tree, int index) {
-    if (index < 0 || index >= tree.size()) throw;
+size_t union_find_tree_find_set(vector<size_t>& tree, size_t index) {
+    if (index >= tree.size()) throw;
     if (tree[index] != index) {
         // Path Compression
         auto root = union_find_tree_find_set(tree, tree[index]);
@@ -23,25 +20,25 @@ int union_find_tree_find_set(vector<int>& tree, int index) {
 
 struct Query {
     // inclusive: [l, r]
-    int l, r, idx;
+    size_t l, r, idx;
 };
 
 // Arpa's trik, O(a(N)) on average
-vector<int> solve(vector<int>& array, vector<pair<int,int>>& queries) {
+vector<int> solve(vector<int>& array, vector<pair<size_t,size_t>>& queries) {
     vector<vector<Query>> queries_by_r(array.size());
-    for (int i = 0; i < queries.size(); ++i) {
+    for (size_t i = 0; i < queries.size(); ++i) {
         auto query = queries[i];
         auto l = query.first;
         auto r = query.second;
         queries_by_r[r].push_back(Query { l, r, i });
     }
     
-    stack<int> s;
+    stack<size_t> s;
     vector<int> ans(array.size());
-    vector<int> tree(array.size());
+    vector<size_t> tree(array.size());
 
     union_find_tree_init(tree);
-    for (int i = 0; i < array.size(); ++i) {
+    for (size_t i = 0; i < array.size(); ++i) {
         while (!s.empty() && array[s.top()] > array[i]) {
             tree[s.top()] = i;
             s.pop();
@@ -57,32 +54,32 @@ vector<int> solve(vector<int>& array, vector<pair<int,int>>& queries) {
 }
 
 int main() {
-    const int n = 10;
-    const int m = 5;
+    const size_t n = 10;
+    const size_t m = 5;
 
     vector<int> array(n);
-    vector<pair<int,int>> queries(m);
+    vector<pair<size_t,size_t>> queries(m);
     random_device random;
 
-    for (int i = 0; i < array.size(); ++i) {
+    for (size_t i = 0; i < array.size(); ++i) {
         array[i] = random() % n;
     }
-    for (int i = 0; i < queries.size(); ++i) {
-        int l = random() % n, r = l + random() % (n - l);
+    for (size_t i = 0; i < queries.size(); ++i) {
+        size_t l = random() % n, r = l + random() % (n - l);
         queries[i] = { l, r };
     }
 
     auto ans = solve(array, queries);
 
     cout << "array" << endl;
-    for (int i = 0; i < array.size(); ++i) {
+    for (size_t i = 0; i < array.size(); ++i) {
         if (i > 0) cout << " ";
         cout << array[i];
     }
     cout << endl;
 
     cout << "queries" << endl;
-    for (int i = 0; i < queries.size(); ++i) {
+    for (size_t i = 0; i < queries.size(); ++i) {
         cout << queries[i].first << " " << queries[i].second << " -> " << ans[i] << endl;
     }
 
