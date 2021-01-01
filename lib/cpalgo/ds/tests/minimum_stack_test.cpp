8a9c9f3b273aa245f767b09cf35239635746d6d5
Taichi Yamakawa <xirckey@gmail.com>
2021-01-01 17:10:07 +0900

    Add tests for minimum-stack

---
+++ lib/cpalgo/ds/tests/minimum_stack_test.cpp
@@ -0,0 +1,86 @@
+#include <bits/stdc++.h>
+#include "gtest/gtest.h"
+#include "cpalgo/ds/minimum-stack.hpp"
+
+
+TEST(MinimumStackTest, IsEmptyInitially) {
+    MinimumStack<int> s;
+    EXPECT_TRUE(s.empty());
+    EXPECT_EQ(0ULL, s.size());
+}
+
+TEST(MinimumStackTest, ShouldPushItems) {
+    MinimumStack<int> s;
+
+    s.push(2);
+    EXPECT_FALSE(s.empty());
+    EXPECT_EQ(1ULL, s.size());
+    EXPECT_EQ(2, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(2, s.top());
+
+    s.push(1);
+    EXPECT_FALSE(s.empty());
+    EXPECT_EQ(2ULL, s.size());
+    EXPECT_EQ(1, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(1, s.top());
+
+    s.push(3);
+    EXPECT_FALSE(s.empty());
+    EXPECT_EQ(3ULL, s.size());
+    EXPECT_EQ(1, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(3, s.top());
+}
+
+TEST(MinimumStackTest, ShouldPopItems) {
+    MinimumStack<int> s;
+
+    s.push(2);
+    s.push(1);
+    s.push(3);
+
+    EXPECT_FALSE(s.empty());
+    EXPECT_EQ(3ULL, s.size());
+    EXPECT_EQ(1, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(3, s.top());
+
+    s.pop();
+    EXPECT_FALSE(s.empty());
+    EXPECT_EQ(2ULL, s.size());
+    EXPECT_EQ(1, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(1, s.top());
+
+    s.pop();
+    EXPECT_FALSE(s.empty());
+    EXPECT_EQ(1ULL, s.size());
+    EXPECT_EQ(2, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(2, s.top());
+
+    s.pop();
+    EXPECT_TRUE(s.empty());
+    EXPECT_EQ(0ULL, s.size());
+}
+
+TEST(MinimumStackTest, ShouldUseCustomComparator) {
+    MinimumStack<int, std::greater<int>> s;
+
+    s.push(2);
+    EXPECT_EQ(2, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(2, s.top());
+
+    s.push(3);
+    EXPECT_EQ(3, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(3, s.top());
+
+    s.push(1);
+    EXPECT_EQ(3, s.minimum());
+    EXPECT_EQ(2, s.bottom());
+    EXPECT_EQ(1, s.top());
+}
