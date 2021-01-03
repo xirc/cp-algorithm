273fd50676595e75e5a178533e44475cc97b5c31
Taichi Yamakawa <xirckey@gmail.com>
2021-01-03 20:01:15 +0900

    Verify lca-tarjan

--- aoj/GRL/GRL5C/main_tarjan.cpp
+++ aoj/GRL/GRL5C/main_tarjan.cpp
@@ -1,138 +1,132 @@
-// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_5_C
+// https://onlinejudge.u-aizu.ac.jp/problems/GRL_5_C
 
 #include <bits/stdc++.h>
 
-// Union Find Tree (Disjoint Set Union)
-// Memory: O(N)
+
 class UnionFindTree {
 public:
-    struct node { int parent, rank; };
+    struct node {
+        size_t leader;
+        size_t rank;
+    };
 
-protected:
-    int N;
+private:
+    size_t N;
     std::vector<node> nodes;
 
 public:
-    // O(N)
-    UnionFindTree(int n = 0)
-        : N(n)
-        , nodes(n)
+    // Time: O(N)
+    UnionFindTree(size_t const N = 0)
+        : N(N)
+        , nodes(N)
     {
-        for (int i = 0; i < N; ++i) {
+        for (size_t i = 0; i < N; ++i) {
             nodes[i] = { i, 0 };
         }
     }
-    // O(1)
-    int size() {
+    // Time: O(1)
+    size_t size() {
         return N;
     }
-    // O(a(N))
-    node find(int v) {
+    // v = [0,N)
+    // Time: O(a(N))
+    node find(size_t const v) {
         throw_if_invalid_index(v);
-        if (v != nodes[v].parent) {
+        if (v != nodes[v].leader) {
             // Path Compression
-            nodes[v] = find(nodes[v].parent);
+            nodes[v] = find(nodes[v].leader);
         }
         return nodes[v];
     }
-    // O(a(N))
-    bool same(int u, int v) {
+    // u = [0,N), v = [0,N)
+    // Time: O(a(N))
+    bool same(size_t const u, size_t const v) {
         throw_if_invalid_index(u);
         throw_if_invalid_index(v);
-        return find(u).parent == find(v).parent;
+        return find(u).leader == find(v).leader;
     }
-    // O(a(N))
-    bool unite(int u, int v) {
+    // u = [0,N), v = [0,N)
+    // Time: O(a(N))
+    bool unite(size_t u, size_t v) {
         throw_if_invalid_index(u);
         throw_if_invalid_index(v);
-        u = find(u).parent;
-        v = find(v).parent;
+        u = find(u).leader;
+        v = find(v).leader;
         if (u == v) {
             return false;
         }
         if (nodes[u].rank < nodes[v].rank) {
             std::swap(u, v);
         }
-        nodes[v].parent = u;
+        nodes[v].leader = u;
         if (nodes[u].rank == nodes[v].rank) {
             nodes[u].rank++;
         }
         return true;
     }
 
-protected:
-    void throw_if_invalid_index(int v) {
-        if (v < 0 || v >= N) throw "index out of range";
+private:
+    void throw_if_invalid_index(size_t const v) {
+        if (v >= N) throw std::out_of_range("index out of range");
     }
 };
 
-// LCA: Lowest Common Ancestor
-// Memory: O(V + E + Q)
-class LCA {
-    struct Query { int u, idx; };
 
-    UnionFindTree dsu;
-    std::vector<bool> visited;
-    std::vector<int> ancestor;
-    std::vector<int> answer;
+inline std::vector<size_t> lca(
+    std::vector<std::vector<size_t>> const& adj,
+    std::vector<std::pair<size_t,size_t>> const& queries
+)
+{
+    const size_t N = adj.size(), M = queries.size();
 
-public:
-    // O(V + Q)
-    std::vector<int> solve(
-        const std::vector<std::vector<int>>& adj,
-        const std::vector<std::pair<int,int>>& queries
-    ) {
-        const int N = adj.size(), M = queries.size();
-        std::vector<std::vector<Query>> Q(N);
-        for (int i = 0; i < M; ++i) {
-            const int u = queries[i].first, v = queries[i].second;
-            Q[u].push_back({ v, i });
-            Q[v].push_back({ u, i });
-        }
-        dsu = UnionFindTree(N);
-        visited.assign(N, false);
-        ancestor.assign(N, -1);
-        answer.assign(M, -1);
-        dfs(adj, Q, 0);
-        return answer;
+    struct Query { size_t u; size_t idx; };
+    std::vector<std::vector<Query>> Q(N);
+    for (size_t i = 0; i < M; ++i) {
+        const size_t u = queries[i].first, v = queries[i].second;
+        Q[u].push_back({ v, i });
+        Q[v].push_back({ u, i });
     }
 
-private:
-    void dfs(
-        const std::vector<std::vector<int>>& adj,
-        const std::vector<std::vector<Query>>& queries,
-        int v
-    ) {
+    UnionFindTree dsu(N);
+    std::vector<bool> visited(N, false);
+    std::vector<size_t> ancestor(N, N);
+    std::vector<size_t> answer(M, N);
+
+    std::function<void(size_t)> const dfs = [&](size_t v) {
         visited[v] = true;
         ancestor[v] = v;
-        for (int u : adj[v]) {
+        for (auto const& u : adj[v]) {
             if (visited[u]) continue;
-            dfs(adj, queries, u);
+            dfs(u);
             dsu.unite(u, v);
-            int c = dsu.find(v).parent;
+            size_t c = dsu.find(v).leader;
             ancestor[c] = v;
         }
-        for (auto query : queries[v]) {
+        for (auto const& query : Q[v]) {
             if (!visited[query.u]) continue;
-            int c = dsu.find(query.u).parent;
+            size_t c = dsu.find(query.u).leader;
             answer[query.idx] = ancestor[c];
         }
-    }
-};
+    };
+    dfs(0);
+
+    return answer;
+}
 
 
 using namespace std;
+
 
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
     
     int N, Q;
-    vector<vector<int>> adj;
-    vector<pair<int,int>> queries;
+    vector<vector<size_t>> adj;
+    vector<pair<size_t,size_t>> queries;
 
     cin >> N;
-    adj.assign(N, vector<int>());
+    adj.assign(N, vector<size_t>());
     for (int i = 0; i < N; ++i) {
         int K, c;
         cin >> K;
@@ -143,8 +137,6 @@ int main() {
         }
     }
 
-    LCA solver;
-
     cin >> Q;
     for (int i = 0; i < Q; ++i) {
         int u, v;
@@ -152,7 +144,7 @@ int main() {
         queries.push_back({u, v});
     }
 
-    auto ans = solver.solve(adj, queries);
+    auto ans = lca(adj, queries);
     for (int i = 0; i < ans.size(); ++i) {
         cout << ans[i] << endl;
     }
