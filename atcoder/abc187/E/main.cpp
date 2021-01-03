88251f4f36d5ca5d408cb28afee6bb9a0f07897b
Taichi Yamakawa <xirckey@gmail.com>
2021-01-03 23:01:08 +0900

    atcoder/abc187E (feedback from editorial)

--- atcoder/abc187/E/main.cpp
+++ atcoder/abc187/E/main.cpp
@@ -1,114 +1,5 @@
 #include <bits/stdc++.h>
 
-class LCA {
-private:
-    size_t N, L;
-    std::vector<size_t> tin, tout;
-    std::vector<std::vector<size_t>> up;
-
-public:
-    // root = [0,N)
-    // Time: O(N logN)
-    LCA(
-        std::vector<std::vector<size_t>> const& adj = {},
-        size_t const root = 0
-    )
-    {
-        build(adj, root);
-    }
-    // root = [0,N)
-    // O(N logN)
-    void build(
-        std::vector<std::vector<size_t>> const& adj,
-        size_t const root = 0
-    )
-    {
-        N = adj.size();
-        L = std::ceil(std::log2(std::max(size_t(1),N))) + 1;
-
-        tin.assign(N, 2 * N);
-        tout.assign(N, 2 * N);
-        up.assign(N, std::vector<size_t>(L, 0));
-
-        if (N > 0) {
-            size_t timer = 0;
-            if (root >= N) throw std::out_of_range("root");
-            dfs_euler(adj, root, root, timer);
-        }
-    }
-    // A LCA node of both the node 'u' and the node 'v'
-    // Time: O(logN)
-    size_t query(size_t u, size_t v) const {
-        if (u >= N) throw std::out_of_range("u");
-        if (v >= N) throw std::out_of_range("v");
-
-        if (is_ancestor(u, v)) return u;
-        if (is_ancestor(v, u)) return v;
-        for (size_t i = 0; i < L; ++i) {
-            if (is_ancestor(up[u][L-1-i], v)) continue;
-            u = up[u][L-1-i];
-        }
-        return up[u][0];
-    }
-    // A highest ancestor node of the node 'u'
-    // where the ancestor is not an ancestor of the node 'v'.
-    // If 'u' is an ancestor of 'v', return 'N'.
-    // Time: O(logN)
-    size_t upper_bound(size_t u, size_t const v) const {
-        if (u >= N) throw std::out_of_range("u");
-        if (v >= N) throw std::out_of_range("v");
-
-        if (is_ancestor(u, v)) return N;
-        for (size_t i = 0; i < L; ++i) {
-            if (is_ancestor(up[u][L-1-i], v)) continue;
-            u = up[u][L-1-i];
-        }
-        return u;
-    }
-    // True if the node 'u' is an ancestor of the node 'v'
-    // u = [0,N), v = [0,N)
-    // Time: O(1)
-    bool is_ancestor(size_t const u, size_t const v) const {
-        if (u >= N) throw std::out_of_range("u");
-        if (v >= N) throw std::out_of_range("v");
-        return tin[u] <= tin[v] && tout[u] >= tout[v];
-    }
-    // The In Index of the Euler Tour
-    // u = [0,N)
-    // The In Index = [0,2*N)
-    size_t euler_in(size_t const u) const {
-        if (u >= N) throw std::out_of_range("u");
-        return tin[u];
-    }
-    // The Out Index of the Euler Tour
-    // u = [0,N)
-    // The Out Index = [0, 2*N)
-    size_t euler_out(size_t const u) const {
-        if (u >= N) throw std::out_of_range("u");
-        return tout[u];
-    }
-    // Time: O(1)
-    size_t size() const {
-        return N;
-    }
-
-private:
-    // Time: O(N logN)
-    void dfs_euler(std::vector<std::vector<size_t>> const& adj, size_t v, size_t p, size_t& timer) {
-        tin[v] = timer++;
-        up[v][0] = p;
-        for (size_t i = 1; i < L; ++i) {
-            up[v][i] = up[up[v][i-1]][i-1];
-        }
-        for (auto const& u : adj[v]) {
-            if (u == p) continue;
-            dfs_euler(adj, u, v, timer);
-        }
-        tout[v] = timer++;
-    }
-};
-
-
 using namespace std;
 using ll = long long;
 
@@ -116,43 +7,60 @@ using ll = long long;
 int N;
 vector<int> A, B;
 
-const int root = 0;
-LCA lca;
-vector<ll> M;
+vector<vector<int>> G;
+vector<int> depth;
+vector<ll> cs;
 
 void build() {
-    vector<vector<size_t>> G(N);
+    // Graph
+    G.assign(N, vector<int>());
     for (int i = 0; i < N - 1; ++i) {
         int a = A[i], b = B[i];
         G[a].push_back(b);
         G[b].push_back(a);
     }
-    lca = LCA(G, root);
-    M.assign(2 * N, 0);
+
+    // BFS
+    depth.assign(N, N);
+    queue<int> Q;
+    Q.push(0);
+    depth[0] = 0;
+    while (Q.size()) {
+        int v = Q.front();
+        Q.pop();
+        for (auto u : G[v]) {
+            if (depth[u] < N) continue;
+            depth[u] = depth[v] + 1;
+            Q.push(u);
+        }
+    }
+
+    // score
+    cs.assign(N, 0);
 }
 
 void update(int a, int b, int x) {
-    if (lca.is_ancestor(b, a)) {
-        int c = lca.upper_bound(a, b);
-        M[lca.euler_in(c)] += x;
-        M[lca.euler_out(c)] -= x;
+    if (depth[a] > depth[b]) {
+        cs[a] += x;
     } else {
-        M[lca.euler_in(root)] += x;
-        M[lca.euler_out(root)] -= x;
-        M[lca.euler_in(b)] -= x;
-        M[lca.euler_out(b)] += x;
+        cs[0] += x;
+        cs[b] -= x;
     }
 }
 
-vector<ll> finalize() {
-    for (int i = 1; i < 2 * N; ++i) {
-        M[i] += M[i-1];
+void finalize() {
+    // BFS
+    queue<int> Q;
+    Q.push(0);
+    while(Q.size()) {
+        int v = Q.front();
+        Q.pop();
+        for (auto u : G[v]) {
+            if (depth[u] <= depth[v]) continue;
+            cs[u] += cs[v];
+            Q.push(u);
+        }
     }
-    vector<ll> cs;
-    for (int i = 0; i < N; ++i) {
-        cs.push_back(M[lca.euler_in(i)]);
-    }
-    return cs;
 }
 
 int main() {
@@ -183,7 +91,7 @@ int main() {
         } else throw;
     }
 
-    auto cs = finalize();
+    finalize();
     for (int i = 0; i < N; ++i) {
         cout << cs[i] << endl;
     }
