c4407348148aab4adf7f69ba77ba22ccf03d5066
Taichi Yamakawa <xirckey@gmail.com>
2021-01-04 18:51:44 +0900

    atcoder/abc081D (feedback from editorial)

--- atcoder/abc081/D/main.cpp
+++ atcoder/abc081/D/main.cpp
@@ -7,44 +7,28 @@ int N;
 vector<int> A;
 
 vector<vector<int>> solve() {
-    int r = 0, q = 0;
-    int maxi = A[0], mini = A[0];
-    for (int i = 1; i < N; ++i) {
-        if (maxi < A[i]) {
-            maxi = A[i];
-            r = i;
-        }
-        if (mini > A[i]) {
-            mini = A[i];
-            q = i;
-        }
-    }
+    int r = distance(A.begin(), max_element(A.begin(),A.end()));
+    int q = distance(A.begin(), min_element(A.begin(),A.end()));
+    int maxi = A[r], mini = A[q];
 
-    // Do nothing
-    if (maxi == mini) {
-        return {};
-    }
+    if (maxi == mini) return {};
 
     vector<vector<int>> ans;
     if (abs(maxi) >= abs(mini)) {
         for (int i = 0; i < N; ++i) {
-            if (i == r) continue;
             A[i] += maxi;
             ans.push_back({ r, i });
         }
         for (int i = 1; i < N; ++i) {
-            if (A[i] >= A[i-1]) continue;
             A[i] += A[i-1];
             ans.push_back({ i - 1, i });
         }
     } else {
         for (int i = 0; i < N; ++i) {
-            if (i == q) continue;
             A[i] += mini;
             ans.push_back({ q, i });
         }
         for (int i = N - 2; i >= 0; --i) {
-            if (A[i] <= A[i+1]) continue;
             A[i] += A[i+1];
             ans.push_back({i + 1, i });
         }
