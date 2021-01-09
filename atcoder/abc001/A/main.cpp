fe1846a32fe057f11e1359c467cce4ff1488659d
Taichi Yamakawa <xirckey@gmail.com>
2021-01-09 22:48:41 +0900

    atcoder/abc001A

---
+++ atcoder/abc001/A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int H1, H2;
+    cin >> H1 >> H2;
+    cout << (H1 - H2) << endl;
+
+    return 0;
+}
