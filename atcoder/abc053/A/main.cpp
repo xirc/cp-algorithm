7a1057c7766404b735b5dabfad32dcf08060a70a
Taichi Yamakawa <xirckey@gmail.com>
2021-01-12 22:04:38 +0900

    atcoder/abc053A

---
+++ atcoder/abc053/A/main.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = long long;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int x;
+    cin >> x;
+    if (x < 1200) {
+        cout << "ABC" << endl;
+    } else {
+        cout << "ARC" << endl;
+    }
+
+    return 0;
+}
