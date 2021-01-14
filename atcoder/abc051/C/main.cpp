4643e1e3efd87d3dc4db2a6e333191df1088e0bf
Taichi Yamakawa <xirckey@gmail.com>
2021-01-14 23:45:29 +0900

    atcoder/abc051C (feedback from editorial)

--- atcoder/abc051/C/main.cpp
+++ atcoder/abc051/C/main.cpp
@@ -5,70 +5,23 @@ using ll = long long;
 
 int sx, sy, tx, ty;
 
-void transpose(vector<char>& route) {
-    if (tx < sx) {
-        for (auto &c : route) {
-            if (c == 'R') c = 'L';
-            else if (c == 'L') c = 'R';
-        }
-    }
-    if (ty < sy) {
-        for (auto &c : route) {
-            if (c == 'U') c = 'D';
-            else if (c == 'D') c = 'U';
-        }
-    }
-}
-
-void rotate(vector<char>& route) {
-    for (auto &c : route) {
-        if (c == 'U') c = 'R';
-        else if (c == 'R') c = 'U';
-        else if (c == 'D') c = 'L';
-        else if (c == 'L') c = 'U';
-    }
-}
-
 string solve() {
-    int W = abs(tx - sx), H = abs(ty - sy);
-    if (W == 0 && H == 0) return "";
-
-    int lx = min(sx, tx), ly = min(sy, ty);
-    int rx = max(sx, tx), ry = min(sy, ty);
+    int W = tx - sx, H = ty - sy;
+    assert(W > 0 && H > 0);
 
     vector<char> route;
-    if (W == 0 || H == 0) {
-        int C = W | H;
-        for (int i = 0; i < C; ++i) route.push_back('U');
-        route.push_back('R');
-        for (int i = 0; i < C; ++i) route.push_back('D');
-        route.push_back('L');
-        route.push_back('L');
-        for (int i = 0; i < C; ++i) route.push_back('U');
-        route.push_back('R');
-        route.push_back('U');
-        route.push_back('R');
-        route.push_back('R');
-        for (int i = 0; i < C + 2; ++i) route.push_back('D');
-        route.push_back('L');
-        route.push_back('L');
-        route.push_back('U');
-        if (H == 0) rotate(route);
-    } else {
-        for (int x = 0; x < W; ++x) route.push_back('R');
-        for (int y = 0; y < H; ++y) route.push_back('U');
-        for (int x = 0; x < W; ++x) route.push_back('L');
-        for (int y = 0; y < H; ++y) route.push_back('D');
-        route.push_back('D');
-        for (int x = 0; x < W + 1; ++x) route.push_back('R');
-        for (int y = 0; y < H + 1; ++y) route.push_back('U');
-        route.push_back('L');
-        route.push_back('U');
-        for (int x = 0; x < W + 1; ++x) route.push_back('L');
-        for (int y = 0; y < H + 1; ++y) route.push_back('D');
-        route.push_back('R');
-    }
-    transpose(route);
+    for (int x = 0; x < W; ++x) route.push_back('R');
+    for (int y = 0; y < H; ++y) route.push_back('U');
+    for (int x = 0; x < W; ++x) route.push_back('L');
+    for (int y = 0; y < H; ++y) route.push_back('D');
+    route.push_back('D');
+    for (int x = 0; x < W + 1; ++x) route.push_back('R');
+    for (int y = 0; y < H + 1; ++y) route.push_back('U');
+    route.push_back('L');
+    route.push_back('U');
+    for (int x = 0; x < W + 1; ++x) route.push_back('L');
+    for (int y = 0; y < H + 1; ++y) route.push_back('D');
+    route.push_back('R');
 
     return string(route.begin(), route.end());
 }
