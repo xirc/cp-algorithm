b6c6e60ec8d4a174f0e3c71b7b98b3c8877d1696
Taichi Yamakawa <xirckey@gmail.com>
2021-01-14 23:51:07 +0900

    atcoder/abc051D (feedback from editorial)

--- atcoder/abc051/D/main.cpp
+++ atcoder/abc051/D/main.cpp
@@ -6,22 +6,11 @@ using ll = long long;
 const int inf = 1e7;
 int N, M;
 vector<vector<int>> G;
-vector<vector<int>> P;
-vector<vector<bool>> U;
-
-void fill(int i, int j) {
-    if (P[i][j] == N) {
-        U[j][j] = U[i][j] = true;
-        return;
-    }
-    int k = P[i][j];
-    fill(i, k);
-    fill(k, j);
-}
+vector<vector<int>> O;
 
 int solve() {
-    P.assign(N, vector<int>(N, N));
-    U.assign(N, vector<bool>(N, false));
+    // COPY
+    O = G;
 
     for (int k = 0; k < N; ++k) {
         for (int i = 0; i < N; ++i) {
@@ -29,25 +18,23 @@ int solve() {
                 if (G[i][k] == inf || G[k][j] == inf) continue;
                 if (G[i][k] + G[k][j] < G[i][j]) {
                     G[i][j] = G[i][k] + G[k][j];
-                    P[i][j] = k;
                 }
             }
         }
     }
 
+    int ans = 0;
     for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            fill(i, j);
+        for (int j = i + 1; j < N; ++j) {
+            if (i == j || O[i][j] == inf) continue;
+            bool use = false;
+            for (int s = 0; s < N; ++s) {
+                if (G[s][i] + O[i][j] == G[s][j]) use = true;
+            }
+            if (!use) ++ans;
         }
     }
-
-    int e = 0;
-    for (int i = 0; i < N; ++i) {
-        for (int j = 0; j < N; ++j) {
-            if (i != j && U[i][j]) ++e;
-        }
-    }
-    return M - (e / 2);
+    return ans;
 }
 
 int main() {
@@ -56,6 +43,7 @@ int main() {
 
     cin >> N >> M;
     G.assign(N, vector<int>(N, inf));
+    for (int i = 0; i < N; ++i) G[i][i] = 0;
     for (int i = 0; i < M; ++i) {
         int a, b, c;
         cin >> a >> b >> c;
