99ffee7d8f20268d2afb556d16000e40a2d9ea06
Taichi Yamakawa <xirckey@gmail.com>
2021-01-16 22:49:40 +0900

    atcoder/abc083B

--- atcoder/abc083/B/main.cpp
+++ atcoder/abc083/B/main.cpp
@@ -3,9 +3,34 @@
 using namespace std;
 using ll = long long;
 
+int N, A, B;
+
+int digsum(int m) {
+    int s = 0;
+    while (m > 0) {
+        s += m % 10;
+        m /= 10;
+    }
+    return s;
+}
+
+int solve() {
+    int sum = 0;
+    for (int i = 1; i <= N; ++i) {
+        int ds = digsum(i);
+        if (ds >= A && ds <= B) {
+            sum += i;
+        }
+    }
+    return sum;
+}
+
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
+
+    cin >> N >> A >> B;
+    cout << solve() << endl;
 
     return 0;
 }
