1610861791511333917c0e731a75f25ac73b6921
Taichi Yamakawa <xirckey@gmail.com>
2021-01-16 22:57:13 +0900

    atcoder/abc083D (feedback from editorial)

--- atcoder/abc083/D/main.cpp
+++ atcoder/abc083/D/main.cpp
@@ -3,38 +3,15 @@
 using namespace std;
 using ll = long long;
 
-int count(string const& S) {
-    int const n = S.size();
-    int ans = 0;
-    int i = 0;
-    int l = 0;
-    int m = 0;
-    while (i < n) {
-        if (S[i] == '1') {
-            if (m == 0) l = i;
-            ++m;
-        } else if (m > 0) {
-            int r = n - l - m;
-            int k = min({ n - l, n - r, l + m, m + r });
-            ans = max(ans, k);
-            m = 0;
-        }
-        ++i;
-    }
-    if (m > 0) {
-        int r = n - l - m;
-        int k = min({ n - l, n - r, l + m, m + r });
-        ans = max(ans, k);
-    }
-    return ans;
-}
-
 int solve(string& S) {
-    int ans = count(S);
-    for (int i = 0; i < S.size(); ++i) {
-        S[i] = S[i] == '0' ? '1' : '0';
+    const int N = S.size();
+    int ans = N;
+    for (int i = 0; i + 1 < N; ++i) {
+        if (S[i] != S[i+1]) {
+            int m = max(i+1,N-(i+1));
+            ans = min(ans, m);
+        }
     }
-    ans = max(ans, count(S));
     return ans;
 }
 
