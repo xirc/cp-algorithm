38f3d99525c32a9c0ce6cabc6970ad3f917ed032
Taichi Yamakawa <xirckey@gmail.com>
2021-01-24 19:49:23 +0900

    atcoder/abc006A (feedback from editorial)

--- atcoder/abc006/A/main.cpp
+++ atcoder/abc006/A/main.cpp
@@ -3,22 +3,13 @@
 using namespace std;
 using ll = int64_t;
 
-bool solve(int N) {
-    if (N % 3 == 0) return true;
-    while (N > 0) {
-        if (N % 10 == 3) return true;
-        N /= 10;
-    }
-    return false;
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
 
     int N;
     cin >> N;
-    auto ans = solve(N) ? "YES" : "NO";
+    auto ans = N % 3 == 0 ? "YES" : "NO";
     cout << ans << endl;
 
     return 0;
