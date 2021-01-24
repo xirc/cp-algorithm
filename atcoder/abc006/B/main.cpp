f76b7988dd40641116024e0e2160e9c1206135df
Taichi Yamakawa <xirckey@gmail.com>
2021-01-24 19:49:37 +0900

    atcoder/abc006B (refactored)

--- atcoder/abc006/B/main.cpp
+++ atcoder/abc006/B/main.cpp
@@ -13,9 +13,9 @@ int main() {
     vector<int> A(N, 0);
     if (N >= 3) {
         A[2] = 1;
-    }
-    for (int i = 3; i < N; ++i) {
-        A[i] = (A[i-1] + A[i-2] + A[i-3]) % MOD;
+        for (int i = 3; i < N; ++i) {
+            A[i] = (A[i-1] + A[i-2] + A[i-3]) % MOD;
+        }
     }
     cout << A[N-1] << endl;
 
