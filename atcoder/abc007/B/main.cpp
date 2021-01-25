931b617a5d9d9d23544277f41b1dd79c459ea61e
Taichi Yamakawa <xirckey@gmail.com>
2021-01-26 00:10:58 +0900

    atcoder/abc007B

---
+++ atcoder/abc007/B/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    string A;
+    cin >> A;
+
+    if ("a" < A) {
+        cout << "a" << endl;
+    } else {
+        cout << -1 << endl;
+    }
+
+    return 0;
+}
