3bc3e1a090beb578c84fad6d87d5d32c93bb1a04
Taichi Yamakawa <xirckey@gmail.com>
2021-02-02 23:42:44 +0900

    atcoder/abc012C

---
+++ atcoder/abc012/C/main.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int N;
+    cin >> N;
+    int D = 2025 - N;
+
+    for (int i = 1; i <= 9; ++i) {
+        for (int j = 1; j <= 9; ++j) {
+            if (i * j == D) {
+                cout << i << " x " << j << endl;
+            }
+        }
+    }
+
+    return 0;
+}
