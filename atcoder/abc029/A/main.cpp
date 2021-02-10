c75c689f9a689def75a0d171fcdaee844843c8ad
Taichi Yamakawa <xirckey@gmail.com>
2021-02-11 00:44:44 +0900

    atcoder/abc029A

---
+++ atcoder/abc029/A/main.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    string W;
+    cin >> W;
+    cout << W << "s" << endl;
+
+    return 0;
+}
