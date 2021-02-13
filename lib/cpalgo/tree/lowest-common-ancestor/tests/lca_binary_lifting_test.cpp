92ff9623ff9d83919c45d1b270d508bdd0c7d9c9
Taichi Yamakawa <xirckey@gmail.com>
2021-02-13 15:38:41 +0900

    Revise test cases of tree/lca

--- lib/cpalgo/tree/lowest-common-ancestor/tests/lca_binary_lifting_test.cpp
+++ lib/cpalgo/tree/lowest-common-ancestor/tests/lca_binary_lifting_test.cpp
@@ -14,6 +14,7 @@ TEST(LCABinaryLiftingTest, CanBuildLater) {
     LCA solver;
     EXPECT_EQ(0ULL, solver.size());
 
+    // lca_test_case1
     vector<vector<size_t>> G(4);
     G[0] = { 1, 2 };
     G[1] = { 3 };
@@ -27,25 +28,31 @@ TEST(LCABinaryLiftingTest, CanBuildLater) {
 }
 
 TEST(LCABinaryLiftingTest, ShouldComputeLCA) {
-    vector<vector<size_t>> G(8);
+    // lca_test_case2
+    vector<vector<size_t>> G(9);
     G[0] = { 1, 2, 3 };
     G[1] = { 4, 5 };
+    G[3] = { 8 };
     G[5] = { 6, 7 };
 
     LCA solver(G);
-    EXPECT_EQ(8ULL, solver.size());
+    EXPECT_EQ(9ULL, solver.size());
 
     EXPECT_EQ(1ULL, solver.query(4, 6));
     EXPECT_EQ(1ULL, solver.query(4, 7));
     EXPECT_EQ(0ULL, solver.query(4, 3));
     EXPECT_EQ(0ULL, solver.query(5, 2));
     EXPECT_EQ(5ULL, solver.query(6, 7));
+    EXPECT_EQ(3ULL, solver.query(8, 3));
+    EXPECT_EQ(0ULL, solver.query(8, 2));
 }
 
 TEST(LCABinaryLiftingTest, ShouldComputeAncestor) {
-    vector<vector<size_t>> G(8);
+    // lca_test_case2
+    vector<vector<size_t>> G(9);
     G[0] = { 1, 2, 3 };
     G[1] = { 4, 5 };
+    G[3] = { 8 };
     G[5] = { 6, 7 };
 
     LCA solver(G);
@@ -56,12 +63,15 @@ TEST(LCABinaryLiftingTest, ShouldComputeAncestor) {
     EXPECT_FALSE(solver.is_ancestor(6, 4));
     EXPECT_TRUE(solver.is_ancestor(1, 7));
     EXPECT_FALSE(solver.is_ancestor(7, 1));
+    EXPECT_FALSE(solver.is_ancestor(2, 8));
 }
 
 TEST(LCABinaryLiftingTest, ShouldComputeUpperBound) {
-    vector<vector<size_t>> G(8);
+    // lca_test_case_2
+    vector<vector<size_t>> G(9);
     G[0] = { 1, 2, 3 };
     G[1] = { 4, 5 };
+    G[3] = { 8 };
     G[5] = { 6, 7 };
 
     LCA solver(G);
@@ -72,30 +82,39 @@ TEST(LCABinaryLiftingTest, ShouldComputeUpperBound) {
     EXPECT_EQ(1ULL, solver.upper_bound(4, 2));
     EXPECT_EQ(1ULL, solver.upper_bound(6, 2));
 
-    EXPECT_EQ(8ULL, solver.upper_bound(1, 7));
-    EXPECT_EQ(8ULL, solver.upper_bound(1, 5));
-    EXPECT_EQ(8ULL, solver.upper_bound(0, 5));
+    EXPECT_EQ(9ULL, solver.upper_bound(1, 7));
+    EXPECT_EQ(9ULL, solver.upper_bound(1, 5));
+    EXPECT_EQ(9ULL, solver.upper_bound(0, 5));
+
+    EXPECT_EQ(1ULL, solver.upper_bound(7, 8));
+    EXPECT_EQ(3ULL, solver.upper_bound(8, 7));
 }
 
 
 TEST(LCABinaryLiftingTest, ShouldComputeEulerTour) {
-    vector<vector<size_t>> G(6);
+    // lca_test_case3
+    vector<vector<size_t>> G(8);
     G[0] = { 1, 2, 3 };
-    G[1] = { 4, 5 };
+    G[1] = { 4 };
+    G[2] = { 5, 6, 7 };
 
     LCA solver(G);
 
-    EXPECT_EQ(0ULL, solver.euler_in(0));
-    EXPECT_EQ(1ULL, solver.euler_in(1));
-    EXPECT_EQ(7ULL, solver.euler_in(2));
-    EXPECT_EQ(9ULL, solver.euler_in(3));
-    EXPECT_EQ(2ULL, solver.euler_in(4));
-    EXPECT_EQ(4ULL, solver.euler_in(5));
+    EXPECT_EQ( 0ULL, solver.euler_in(0));
+    EXPECT_EQ( 1ULL, solver.euler_in(1));
+    EXPECT_EQ( 5ULL, solver.euler_in(2));
+    EXPECT_EQ(13ULL, solver.euler_in(3));
+    EXPECT_EQ( 2ULL, solver.euler_in(4));
+    EXPECT_EQ( 6ULL, solver.euler_in(5));
+    EXPECT_EQ( 8ULL, solver.euler_in(6));
+    EXPECT_EQ(10ULL, solver.euler_in(7));
 
-    EXPECT_EQ(11ULL, solver.euler_out(0));
-    EXPECT_EQ(6ULL, solver.euler_out(1));
-    EXPECT_EQ(8ULL, solver.euler_out(2));
-    EXPECT_EQ(10ULL, solver.euler_out(3));
-    EXPECT_EQ(3ULL, solver.euler_out(4));
-    EXPECT_EQ(5ULL, solver.euler_out(5));
+    EXPECT_EQ(15ULL, solver.euler_out(0));
+    EXPECT_EQ( 4ULL, solver.euler_out(1));
+    EXPECT_EQ(12ULL, solver.euler_out(2));
+    EXPECT_EQ(14ULL, solver.euler_out(3));
+    EXPECT_EQ( 3ULL, solver.euler_out(4));
+    EXPECT_EQ( 7ULL, solver.euler_out(5));
+    EXPECT_EQ( 9ULL, solver.euler_out(6));
+    EXPECT_EQ(11ULL, solver.euler_out(7));
 }
--- lib/cpalgo/tree/lowest-common-ancestor/tests/lca_tarjan_test.cpp
+++ lib/cpalgo/tree/lowest-common-ancestor/tests/lca_tarjan_test.cpp
@@ -6,9 +6,11 @@ using namespace std;
 
 
 TEST(LCATarjanTest, ShouldComputeLCA) {
-    vector<vector<size_t>> G(8);
+    // lca_test_case2
+    vector<vector<size_t>> G(9);
     G[0] = { 1, 2, 3 };
     G[1] = { 4, 5 };
+    G[3] = { 8 };
     G[5] = { 6, 7 };
 
     vector<pair<size_t,size_t>> Q;
@@ -17,6 +19,8 @@ TEST(LCATarjanTest, ShouldComputeLCA) {
     Q.push_back({ 4, 3 });
     Q.push_back({ 5, 2 });
     Q.push_back({ 6, 7 });
+    Q.push_back({ 8, 3 });
+    Q.push_back({ 8, 2 });
 
     auto result = lca(G, Q);
     EXPECT_EQ(1ULL, result[0]);
@@ -24,5 +28,7 @@ TEST(LCATarjanTest, ShouldComputeLCA) {
     EXPECT_EQ(0ULL, result[2]);
     EXPECT_EQ(0ULL, result[3]);
     EXPECT_EQ(5ULL, result[4]);
+    EXPECT_EQ(3ULL, result[5]);
+    EXPECT_EQ(0ULL, result[6]);
 }
 
