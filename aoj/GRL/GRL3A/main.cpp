b3a32de2e2b7c325a81a357dde3e0e5e353cc947
Taichi Yamakawa <xirckey@gmail.com>
2021-02-21 19:23:15 +0900

    aoj/GRL3A

--- aoj/GRL/GRL3A/main.cpp
+++ aoj/GRL/GRL3A/main.cpp
@@ -1,71 +1,66 @@
-// http://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=GRL_3_A
+// https://onlinejudge.u-aizu.ac.jp/problems/GRL_3_A
 
-#include <vector>
-#include <algorithm>
+#include <bits/stdc++.h>
 
-// Articulation Points of a Graph
-// Memory: O(V + E)
-// NOTE: undirected, multi-edge, no-self-loop
 class ArticulationPoints {
-    struct edge { int id, to; };
+    struct edge { size_t id, to; };
 
-    int N, M;
+    size_t N, M;
     std::vector<std::vector<edge>> adj;
+
     // Temporary
-    int K;
-    std::vector<int> ord;
-    std::vector<int> low;
-    std::vector<int> ans;
+    size_t K;
+    std::vector<size_t> tin;
+    std::vector<size_t> low;
+    std::vector<size_t> ans;
 
 public:
     // O(V)
-    ArticulationPoints(int n): N(n), M(0), adj(n) {}
+    ArticulationPoints(size_t n = 0): N(n), M(0), adj(n) {}
     // O(1)
-    int size() {
+    size_t size() const {
         return N;
     }
+    // u = [0,N), v = [0,N)
     // O(1)
-    void add_edge(int u, int v) {
-        throw_if_invalid_index(u);
-        throw_if_invalid_index(v);
+    void add_edge(size_t u, size_t v) {
+        if (u >= N) throw std::out_of_range("u");
+        if (v >= N) throw std::out_of_range("v");
         adj[u].push_back({ M, v });
         adj[v].push_back({ M, u });
         M++;
     }
     // O(V + E)
-    std::vector<int> solve() {
+    std::vector<size_t> solve() {
         K = 0;
-        ord.assign(N, -1);
-        low.assign(N, -1);
+        tin.assign(N, N);
+        low.assign(N, N);
         ans.clear();
-        for (int u = 0; u < N; ++u) {
-            if (ord[u] != -1) continue;
-            dfs(u);
+        for (size_t u = 0; u < N; ++u) {
+            if (tin[u] != N) continue;
+            dfs(u, { M, N });
         }
         return ans;
     }
 
 private:
-    void throw_if_invalid_index(int index) {
-        if (index < 0 || index > N) throw "index out of range";
-    }
-    void dfs(int u, const edge& ein = { -1, -1 }) {
+    void dfs(size_t u, edge const& ein) {
         bool is_cutpoint = false;
-        int childlen = 0;
-        ord[u] = low[u] = K++;
-        for (auto& e : adj[u]) {
-            if (ord[e.to] == -1) {
+        size_t childlen = 0;
+        tin[u] = low[u] = K++;
+        for (auto const& e : adj[u]) {
+            if (tin[e.to] == N) {
                 dfs(e.to, e);
                 low[u] = std::min(low[u], low[e.to]);
                 ++childlen;
-                if (ein.id != -1 && ord[u] <= low[e.to]) {
+                if (ein.id != M && tin[u] <= low[e.to]) {
                     is_cutpoint = true;
                 }
             } else if (e.id != ein.id) {
-                low[u] = std::min(low[u], ord[e.to]);
+                low[u] = std::min(low[u], tin[e.to]);
             }
         }
-        if (ein.id == -1 && childlen > 1) {
+        if (ein.id == M && childlen > size_t(1)) {
             is_cutpoint = true;
         }
         if (is_cutpoint) {
@@ -73,10 +68,6 @@ private:
         }
     }
 };
-
-
-#include <iostream>
-#include <algorithm>
 
 using namespace std;
 
