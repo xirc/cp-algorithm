e622629a33d6df96e3fbedf928a1044e2eae24f8
Taichi Yamakawa <xirckey@gmail.com>
2021-03-09 23:34:36 +0900

    atcoder/abc041C

---
+++ atcoder/abc041/C/main.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int N;
+    vector<array<int,2>> A;
+
+    cin >> N;
+    A.assign(N, { 0, 0 });
+    for (int i = 0; i < N; ++i) {
+        cin >> A[i][0];
+        A[i][1] = i + 1;
+    }
+    sort(A.begin(), A.end());
+    reverse(A.begin(), A.end());
+    for (auto const& e : A) {
+        cout << e[1] << endl;
+    }
+
+    return 0;
+}
