e268f8bdc6de255a8df4d91316982cae7806fd88
Taichi Yamakawa <xirckey@gmail.com>
2021-03-11 23:08:35 +0900

    atcoder/abc048D (feedback from editorial)

--- atcoder/abc048/D/main.cpp
+++ atcoder/abc048/D/main.cpp
@@ -7,29 +7,11 @@ using ff = long double;
 bool solve(string const& s) {
     const int N = s.size();
     auto a = s[0], b = s[s.size()-1];
-    int m = 0;
     if (a == b) {
-        int len = 0;
-        for (int i = 1; i < N; ++i) {
-            if (s[i] != a){
-                ++len;
-                continue;
-            }
-            m += len - 1;
-            len = 0;
-        }
+        return s.size() % 2 == 0;
     } else { // a != b
-        bool useB = true;
-        for (int i = 1; i < N; ++i) {
-            auto c = useB ? b : a;
-            if (s[i] != c) {
-                ++m;
-            } else {
-                useB ^= true;
-            }
-        }
+        return s.size() % 2 == 1;
     }
-    return m % 2 == 1;
 }
 
 int main() {
