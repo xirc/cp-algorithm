855ebf5c6f6715a7ee25e07003e65cbc753cc885
Taichi Yamakawa <xirckey@gmail.com>
2021-03-13 22:57:08 +0900

    atcoder/abc049B

---
+++ atcoder/abc049/B/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int H, W;
+    cin >> H >> W;
+    string C;
+    for (int i = 0; i < H; ++i) {
+        cin >> C;
+        cout << C << endl;
+        cout << C << endl;
+    }
+
+    return 0;
+}
