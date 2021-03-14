bdc42719f9436cf8829100c25c7d808f2984ce2e
Taichi Yamakawa <xirckey@gmail.com>
2021-03-14 23:18:15 +0900

    atcoder/abc042B

---
+++ atcoder/abc042/B/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int N, L;
+    vector<string> S;
+    
+    cin >> N >> L;
+    S.assign(N, "");
+    for (int i = 0; i < N; ++i) {
+        cin >> S[i];
+    }
+    sort(S.begin(), S.end());
+    for (int i = 0; i < N; ++i) {
+        cout << S[i];
+    }
+    cout << endl;
+
+    return 0;
+}
