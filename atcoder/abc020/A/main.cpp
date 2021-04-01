ff36e8dd1af4871d3c6e1490d3cb8022c5182a8d
Taichi Yamakawa <xirckey@gmail.com>
2021-04-01 22:59:49 +0900

    atcoder/abc020A

---
+++ atcoder/abc020/A/main.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int Q;
+    cin >> Q;
+    if (Q == 1) {
+        cout << "ABC" << endl;
+    } else if (Q == 2) {
+        cout << "chokudai" << endl;
+    }
+
+    return 0;
+}
