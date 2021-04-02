0de80fce6f7af5b1867634d928db7666f97c6eef
Taichi Yamakawa <xirckey@gmail.com>
2021-04-03 00:16:18 +0900

    atcoder/abc008C

--- atcoder/abc008/C/main.cpp
+++ atcoder/abc008/C/main.cpp
@@ -4,37 +4,21 @@ using namespace std;
 using ll = int64_t;
 using ff = long double;
 
-ll factorial(int n) {
-    ll f = 1;
-    while (n > 0) {
-        f *= n;
-        --n;
-    }
-    return f;
-}
-
 int N;
 vector<int> C;
 
 ff solve() {
-    ll score = 0;
-    vector<array<int,2>> cs(N, { 0, 0 });
+    ff exp = 0;
     for (int i = 0; i < N; ++i) {
-        cs[i] = { C[i], i };
-    }
-    sort(cs.begin(), cs.end());
-    do {
-        for (int i = 0; i < N; ++i) {
-            int numDiv = 0;
-            for (int j = 0; j < i; ++j) {
-                if (cs[i][0] % cs[j][0] == 0) numDiv++;
-            }
-            if (numDiv % 2 == 0) {
-                score++;
-            }
+        int M = 0;
+        for (int j = 0; j < N; ++j) {
+            if (i == j) continue;
+            if (C[i] % C[j] == 0) ++M;
         }
-    } while (next_permutation(cs.begin(), cs.end()));
-    return score / ff(factorial(N));
+        ff px = ff(M / 2 + 1) / (M + 1);
+        exp += px;
+    }
+    return exp;
 }
 
 int main() {
