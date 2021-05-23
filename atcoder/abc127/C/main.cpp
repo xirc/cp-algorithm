88998f32b5dd54bdd485b0f9bbed26efcfc22a8a
Taichi Yamakawa <xirckey@gmail.com>
2021-05-23 21:31:04 +0900

    atcoder/abc127C (feedback from editorial)

--- atcoder/abc127/C/main.cpp
+++ atcoder/abc127/C/main.cpp
@@ -19,18 +19,9 @@ int main() {
         --L[i], --R[i];
     }
 
-    vector<int> A(N+1, 0);
-    for (int i = 0; i < M; ++i) {
-        A[L[i]]++;
-        A[R[i]+1]--;
-    }
-    for (int i = 1; i < N; ++i) {
-        A[i] += A[i-1];
-    }
-    int ans = 0;
-    for (int i = 0; i < N; ++i) {
-        if (A[i] == M) ++ans;
-    }
+    int l = *max_element(L.begin(), L.end());
+    int r = *min_element(R.begin(), R.end());
+    int ans = max(r - l + 1, 0);
     cout << ans << endl;
 
     return 0;
