015a73a7e9426997a9ce432b6c0b42f0db528204
Taichi Yamakawa <xirckey@gmail.com>
2021-06-15 19:00:58 +0900

    atcoder/abc136A

---
+++ atcoder/abc136/A/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int A, B, C;
+    cin >> A >> B >> C;
+    cout << max(0, C - (A - B)) << endl;
+
+    return 0;
+}
