d808175701b46c7a51fb5a75717fdec930d523ba
Taichi Yamakawa <xirckey@gmail.com>
2021-07-14 21:10:07 +0900

    atcoder/abc151A

---
+++ atcoder/abc151/A/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    char c;
+    cin >> c;
+    cout << char(c + 1) << endl;
+
+    return 0;
+}
