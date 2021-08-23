902e350115d0104363a46b20e1a075dae8f805f7
Taichi Yamakawa <xirckey@gmail.com>
2021-08-23 20:09:28 +0900

    atcoder/abc172D (feedback from editorial)

--- atcoder/abc172/D/main.cpp
+++ atcoder/abc172/D/main.cpp
@@ -4,30 +4,11 @@ using namespace std;
 using ll = int64_t;
 using ff = long double;
 
-int count(int N, int b) {
-    int c = 0;
-    while (N % b == 0) {
-        ++c;
-        N /= b;
-    }
-    return c;
-}
-
 ll solve(int const N) {
-    vector<int> divisors(N + 1, 1);
-    vector<bool> is_prime(N + 1, true);
-    is_prime[0] = is_prime[1] = false;
-    for (int p = 2; p <= N; ++p) {
-        if (!is_prime[p]) continue;
-        divisors[p] = 2;
-        for (int q = 2 * p; q <= N; q += p) {
-            is_prime[q] = false;
-            divisors[q] *= count(q, p) + 1;
-        }
-    }
     ll ans = 0;
     for (int x = 1; x <= N; ++x) {
-        ans += ll(x) * divisors[x];
+        int y = N / x;
+        ans += ll(1) * y * (y + 1) * x / 2;
     }
     return ans;
 }
