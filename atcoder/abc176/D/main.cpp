f5b58d939909392399870557e0699a1f72eab52d
Taichi Yamakawa <xirckey@gmail.com>
2021-08-28 23:50:08 +0900

    atcoder/abc176D (feedback from editorial)
    0-1 BFS

--- atcoder/abc176/D/main.cpp
+++ atcoder/abc176/D/main.cpp
@@ -15,25 +15,21 @@ int solve() {
     vector<int> const dx = { 1, -1, 0, 0 };
     vector<int> const dy = { 0, 0, 1, -1 };
 
-    priority_queue<P, vector<P>, greater<P>> Q;
-    vector<vector<bool>> F(H, vector<bool>(W, false));
-    vector<vector<int>> D(H, vector<int>(W, inf));
-    D[sy][sx] = 0;
-    Q.push({ 0, sy, sx });
+    deque<P> Q;
+    vector<vector<bool>> D(H, vector<bool>(W, false));
+    Q.push_front({ 0, sy, sx });
     while (Q.size()) {
-        auto p = Q.top(); Q.pop();
-        int y = p[1], x = p[2];
-        if (y == ey && x == ex) return D[y][x];
-        if (F[y][x]) continue;
+        auto p = Q.front(); Q.pop_front();
+        int y = p[1], x = p[2], d = p[0];
+        if (y == ey && x == ex) return d;
+        if (D[y][x]) continue;
         for (int i = 0; i < 4; ++i) {
             int ny = y + dy[i], nx = x + dx[i];
             if (ny < 0 || ny >= H) continue;
             if (nx < 0 || nx >= W) continue;
             if (G[ny][nx] == '#') continue;
-            if (D[ny][nx] > D[y][x]) {
-                D[ny][nx] = D[y][x];
-                Q.push({ D[y][x], ny, nx });
-            }
+            if (D[ny][nx]) continue;
+            Q.push_front({ d, ny, nx });
         }
         for (int dy = -2; dy <= 2; ++dy) {
             for (int dx = -2; dx <= 2; ++dx) {
@@ -41,13 +37,11 @@ int solve() {
                 if (ny < 0 || ny >= H) continue;
                 if (nx < 0 || nx >= W) continue;
                 if (G[ny][nx] == '#') continue;
-                if (D[ny][nx] > D[y][x] + 1) {
-                    D[ny][nx] = D[y][x] + 1;
-                    Q.push({ D[y][x] + 1, ny, nx });
-                }
+                if (D[ny][nx]) continue;
+                Q.push_back({ d + 1, ny, nx });
             }
         }
-        F[y][x] = true;
+        D[y][x] = true;
     }
     return -1;
 }
