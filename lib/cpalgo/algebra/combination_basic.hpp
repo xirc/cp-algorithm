2f97ac4815642ad1bd02532ee80c44040a9f3a92
Taichi Yamakawa <xirckey@gmail.com>
2021-08-29 16:20:43 +0900

    CombinationBasic uses an inclusive upper bound

--- lib/cpalgo/algebra/combination_basic.hpp
+++ lib/cpalgo/algebra/combination_basic.hpp
@@ -40,25 +40,25 @@ public:
     void build(size_t const N) {
         this->N = N;
 
-        factorial.assign(N, 0);
-        factorial_inverse.assign(N, 0);
-        inverse.assign(N, 0);
+        factorial.assign(N + 1, 0);
+        factorial_inverse.assign(N + 1, 0);
+        inverse.assign(N + 1, 0);
         factorial[0] = factorial[1] = 1;
         factorial_inverse[0] = factorial_inverse[1] = 1;
         inverse[0] = 0;
         inverse[1] = 1;
-        for (size_t i = 2; i < N; ++i) {
+        for (size_t i = 2; i <= N; ++i) {
             factorial[i] = factorial[i-1] * i % MOD;
             inverse[i] = MOD - inverse[MOD % i] * (MOD / i) % MOD;
             factorial_inverse[i] = factorial_inverse[i-1] * inverse[i] % MOD;
         }
     }
     // nCk
-    // n = [0,N), k = [0,n]
+    // n = [0,N], k = [0,n]
     // Time: O(1)
     uint64_t operator()(size_t const n, size_t const k) const {
         if (k > n) throw std::out_of_range("k");
-        if (n >= N) throw std::out_of_range("n");
+        if (n > N) throw std::out_of_range("n");
         return factorial[n] * (factorial_inverse[k] * factorial_inverse[n - k] % MOD) % MOD;
     }
     // Time: O(1)
