2f3cc4d8bcfc494c81d3c355af5a898098d98ec2
Taichi Yamakawa <xirckey@gmail.com>
2021-08-29 18:17:27 +0900

    Revise example of CombinationBasic

--- lib/main/algebra/main-combination-basic.cpp
+++ lib/main/algebra/main-combination-basic.cpp
@@ -1,16 +1,58 @@
 #include <bits/stdc++.h>
 #include "template/template-main.hpp"
 #include "cpalgo/algebra/combination_basic.hpp"
-#include "./template-combination-interp.hpp"
 
 using namespace std;
 
-CombinationBasic solver;
+CombinationBasic<> solver;
+
+void action_init() {
+    size_t N;
+    std::cin >> N;
+    solver = CombinationBasic<>(N);
+}
+
+void action_eval_C() {
+    size_t n, k;
+    std::cin >> n >> k;
+    if (n > solver.size() || n < k) {
+        std::cout << "false" << std::endl;
+        return;
+    }
+    auto ans = solver.C(n, k);
+    std::cout << ans << std::endl;
+}
+
+void action_eval_P() {
+    size_t n, k;
+    std::cin >> n >> k;
+    if (n > solver.size() || n < k) {
+        std::cout << "false" << std::endl;
+        return;
+    }
+    auto ans = solver.P(n, k);
+    std::cout << ans << std::endl;
+}
+
+void action_eval_H() {
+    size_t n, k;
+    std::cin >> n >> k;
+    if ((n == 0 && k > 0) || (n > 0 && n + k - 1 > solver.size())) {
+        std::cout << "false" << std::endl;
+        return;
+    }
+    auto ans = solver.H(n, k);
+    std::cout << ans << std::endl;
+}
 
 void setup(string& header, map<string,Command>& commands) {
-    header = "Binomial Coefficient";
+    header = "Binomial Coefficient in modulo";
     commands["init"] =
-        Command { "init {N} {MOD}", bind(action_init<CombinationBasic>, solver) };
-    commands["eval"] =
-        Command { "eval {n} {k}", bind(action_eval<CombinationBasic>, solver) };
+        Command { "init {N}", action_init };
+    commands["C"] =
+        Command { "C {n} {k}", action_eval_C };
+    commands["P"] =
+        Command { "P {n} {k}", action_eval_P };
+    commands["H"] =
+        Command { "H {n} {k}", action_eval_H };
 }
