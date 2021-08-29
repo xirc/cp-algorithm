047fecc5fda1f308ec57e67b697cea815bbca42b
Taichi Yamakawa <xirckey@gmail.com>
2021-08-29 18:24:47 +0900

    atcoder/abc110D
    Verify CombinationBasic

--- atcoder/abc110/D/main.cpp
+++ atcoder/abc110/D/main.cpp
@@ -1,44 +1,58 @@
 #include <bits/stdc++.h>
 
-class Combination {
+template <uint64_t MOD = 1000000007>
+class CombinationBasic {
     size_t N;
-    uint64_t MOD;
     std::vector<uint64_t> factorial, inverse, factorial_inverse;
 
 public:
-    // O(N)
-    // MOD should be a prime number
-    Combination(
-        size_t const N = 10000000,
-        uint64_t const MOD = 1000000007
+    // Time: O(N)
+    CombinationBasic(
+        size_t const N = 10000000
     )
     {
-        build(N, MOD);
+        build(N);
     }
     // Time: O(N)
-    void build(size_t const N, uint64_t const MOD) {
+    void build(size_t const N) {
         this->N = N;
-        this->MOD = MOD;
-
-        factorial.assign(N, 0);
-        factorial_inverse.assign(N, 0);
-        inverse.assign(N, 0);
+        factorial.assign(N + 1, 0);
+        factorial_inverse.assign(N + 1, 0);
+        inverse.assign(N + 1, 0);
         factorial[0] = factorial[1] = 1;
         factorial_inverse[0] = factorial_inverse[1] = 1;
         inverse[0] = 0;
         inverse[1] = 1;
-        for (size_t i = 2; i < N; ++i) {
+        for (size_t i = 2; i <= N; ++i) {
             factorial[i] = factorial[i-1] * i % MOD;
             inverse[i] = MOD - inverse[MOD % i] * (MOD / i) % MOD;
             factorial_inverse[i] = factorial_inverse[i-1] * inverse[i] % MOD;
         }
     }
     // nCk
+    // n = [0,N], k = [0,n]
     // Time: O(1)
-    uint64_t operator()(size_t const n, size_t const k) const {
+    uint64_t C(size_t const n, size_t const k) const {
         if (k > n) throw std::out_of_range("k");
-        if (n >= N) throw std::out_of_range("n");
+        if (n > N) throw std::out_of_range("n");
         return factorial[n] * (factorial_inverse[k] * factorial_inverse[n - k] % MOD) % MOD;
+    }
+    // nPk
+    // P(n,k) = n! / (n-k)!
+    // n = [0,N], k = [0,n]
+    // Time: O(1)
+    uint64_t P(size_t const n, size_t const k) const {
+        if (k > n) throw std::out_of_range("k");
+        if (n > N) throw std::out_of_range("n");
+        return factorial[n] * factorial_inverse[n-k] % MOD;
+    }
+    // nHk
+    // H(n,k) = C(n+k-1,k)
+    // n = [0,N-k+1], k = [0,N]
+    // Time: O(1)
+    uint64_t H(size_t const n, size_t const k) const {
+        if (n == 0 && k == 0) return 1;
+        return C(n+k-1, k);
     }
     // Time: O(1)
     size_t size() const {
@@ -64,8 +78,8 @@ inline std::vector<uint64_t> prime_factorization(uint64_t M) {
 using namespace std;
 using ll = long long;
 
-const ll MOD = 1e9+7;
-Combination comb(200000, MOD);
+ll const MOD = 1e9+7;
+CombinationBasic<MOD> solver(200000);
 
 ll solve(int N, int M) {
     auto factors = prime_factorization(M);
@@ -78,7 +92,7 @@ ll solve(int N, int M) {
     ll ans = 1;
     for (auto f : counter) {
         auto e = f.second;
-        ans *= comb(e + N - 1, e);
+        ans *= solver.C(e + N - 1, e);
         ans %= MOD;
     }
     return ans;
