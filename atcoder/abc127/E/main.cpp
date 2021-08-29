e496f3a8c54392ea628b3678e565739ea2a92e3b
Taichi Yamakawa <xirckey@gmail.com>
2021-08-29 15:50:35 +0900

    atcoder/abc127E
    Verify modint<>

--- atcoder/abc127/E/main.cpp
+++ atcoder/abc127/E/main.cpp
@@ -47,15 +47,16 @@ public:
     }
 };
 
-template<std::uint64_t MOD>
+template<std::int64_t MOD = 1000000007>
 class modint {
-    using u64 = std::uint64_t;
-    u64 x;
+    using i64 = std::int64_t;
+    i64 x;
 
 public:
-    constexpr modint(u64 const& x = 0) noexcept : x(x % MOD) {}
-    constexpr u64& value() noexcept { return x; }
-    constexpr const u64& value() const noexcept { return x; }
+    constexpr modint(i64 const& x = 0) noexcept : x(((x % MOD) + MOD) % MOD) {}
+    constexpr i64& value() noexcept { return x; }
+    constexpr const i64& value() const noexcept { return x; }
+    constexpr i64 modulo() const noexcept { return MOD; }
     constexpr modint operator+(modint const& rhs) const noexcept { return modint(*this) += rhs; }
     constexpr modint operator-(modint const& rhs) const noexcept { return modint(*this) -= rhs; }
     constexpr modint operator*(modint const& rhs) const noexcept { return modint(*this) *= rhs; }
@@ -76,7 +77,7 @@ public:
     }
     constexpr modint& operator/=(modint rhs) noexcept {
         // Use Fermat's little theorem & binary exponentiation
-        u64 exp = MOD - 2;
+        auto exp = MOD - 2;
         while (exp) {
             if (exp & 1) *this *= rhs;
             rhs *= rhs;
