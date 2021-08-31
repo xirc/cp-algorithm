710ad1df7d73222c36fd66e74377c9c094830ca2
Taichi Yamakawa <xirckey@gmail.com>
2021-08-31 20:13:51 +0900

    atcoder/abc178A

---
+++ atcoder/abc178/A/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int x;
+    cin >> x;
+    cout << (x ? 0 : 1) << endl;
+
+    return 0;
+}
