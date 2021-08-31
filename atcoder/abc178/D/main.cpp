c8f5722d9cd2230ba7fb2f40cee947cd0b618a21
Taichi Yamakawa <xirckey@gmail.com>
2021-08-31 20:21:37 +0900

    atcoder/abc178D (feedback from editorial)

--- atcoder/abc178/D/main.cpp
+++ atcoder/abc178/D/main.cpp
@@ -4,22 +4,14 @@ using namespace std;
 using ll = int64_t;
 using ff = long double;
 
-int const N = 2001;
-ll const MOD = 1e9+7;
-vector<bool> done(N, false);
-vector<int> value(N, 0);
 ll solve(int S) {
-    if (S < 3) return 0;
-    if (done[S]) return value[S];
-    ll ans = 1;
-    for (int x = 3; x <= S; ++x) {
-        int y = S - x;
-        ans += solve(y);
-        ans %= MOD;
+    int const MOD = 1e9+7;
+    vector<int> DP(2001, 0);
+    DP[0] = 1; DP[1] = DP[2] = 0;
+    for (int i = 3; i <= S; ++i) {
+        DP[i] = (DP[i-1] + DP[i-3]) % MOD;
     }
-    done[S] = true;
-    value[S] = ans;
-    return ans;
+    return DP[S];
 }
 
 int main() {
