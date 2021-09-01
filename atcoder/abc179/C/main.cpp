e8d4af4ace94c40854c73afe6d716c806cb6f3d1
Taichi Yamakawa <xirckey@gmail.com>
2021-09-02 00:28:00 +0900

    atcoder/abc179C (feedback from editorial)

--- atcoder/abc179/C/main.cpp
+++ atcoder/abc179/C/main.cpp
@@ -5,29 +5,11 @@ using ll = int64_t;
 using ff = long double;
 
 ll solve(int const N) {
-    vector<bool> is_prime(N + 1, true);
-    vector<int> patterns(N + 1, 1);
-    is_prime[0] = is_prime[1] = false;
-    patterns[0] = 0;
-    for (int p = 2; p <= N; ++p) {
-        if (!is_prime[p]) continue;
-        patterns[p] *= 2;
-        for (int q = 2 * p; q <= N; q += p) {
-            is_prime[q] = false;
-            int e = 0;
-            int m = q;
-            while (m % p == 0) {
-                ++e;
-                m /= p;
-            }
-            patterns[q] *= e + 1;
-        }
+    ll num_patterns = 0;
+    for (int A = 1; A < N; ++A) {
+        num_patterns += (N - 1) / A;
     }
-    ll all_patterns = 0;
-    for (int c = 1; c <= N; ++c) {
-        all_patterns += patterns[N - c];
-    }
-    return all_patterns;
+    return num_patterns;
 }
 
 int main() {
