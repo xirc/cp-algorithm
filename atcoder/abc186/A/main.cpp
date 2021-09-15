2c1d3165bb0e9a8497eebb2ddba5ffa1591c26a2
Taichi Yamakawa <xirckey@gmail.com>
2021-09-15 22:01:09 +0900

    atcoder/abc186A

---
+++ atcoder/abc186/A/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int N, W;
+    cin >> N >> W;
+    int C = N / W;
+    cout << C << endl;
+
+    return 0;
+}
