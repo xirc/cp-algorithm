3294527e8e0c49b53d0ffc3cbca0944354691cef
Taichi Yamakawa <xirckey@gmail.com>
2021-09-16 22:44:40 +0900

    atcoder/abc188C (feedback from editorial)

--- atcoder/abc188/C/main.cpp
+++ atcoder/abc188/C/main.cpp
@@ -4,25 +4,12 @@ using namespace std;
 using ll = int64_t;
 using ff = long double;
 
-int N, M;
-vector<int> A;
-
-array<int,2> solve(int tl, int tr) {
-    if (tr - tl == 2) {
-        if (A[tl] < A[tl+1]) return { tl, tl+1 };
-        else return { tl+1, tl };
-    }
-    int tm = (tl + tr) >> 1;
-    auto lhs = solve(tl, tm);
-    auto rhs = solve(tm, tr);
-    int l = lhs[1], r = rhs[1];
-    if (A[l] < A[r]) return { l, r };
-    else return { r, l };
-}
-
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0); cout.tie(0);
+
+    int N, M;
+    vector<int> A;
 
     cin >> N;
     M = 1 << N;
@@ -30,8 +17,12 @@ int main() {
     for (int i = 0; i < M; ++i) {
         cin >> A[i];
     }
-    int ans = solve(0, M)[0] + 1;
-    cout << ans << endl;
+
+    M /= 2;
+    auto l = distance(A.begin(), max_element(A.begin(), A.begin() + M));
+    auto r = distance(A.begin(), max_element(A.begin() + M, A.end()));
+    int ans = (A[l] < A[r]) ? l : r;
+    cout << (ans+1) << endl;
 
     return 0;
 }
