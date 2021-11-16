f171f44b21276f554de4571a0bb4bab9f9d7f653
Taichi Yamakawa <xirckey@gmail.com>
2021-11-16 21:01:03 +0900

    atcoder/abc211A

---
+++ atcoder/abc211/A/main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int A, B;
+    cin >> A >> B;
+    ff C = ff(A + 2 * B) / 3;
+    cout << fixed << setprecision(6) << C << endl;
+
+    return 0;
+}
