9bf8b28d7a6838812e4509a7815395b87d1cca4d
Taichi Yamakawa <xirckey@gmail.com>
2021-12-22 19:13:26 +0900

    atcoder/abc222A

---
+++ atcoder/abc222/A/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+using ll = int64_t;
+using ff = long double;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0); cout.tie(0);
+
+    int N;
+    cin >> N;
+    cout << setfill('0') << setw(4) << N << endl;
+
+    return 0;
+}
